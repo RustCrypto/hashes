1783a612598ffe64fdef7512ba6df633a54fc57a
felipeamp <felipeamp@gmail.com>
2017-01-11 13:35:42 -0200

    Added one_million_a test to MD2

--- md2/tests/lib.rs
+++ md2/tests/lib.rs
@@ -3,11 +3,17 @@
 extern crate crypto_tests;
 extern crate md2;
 
-use crypto_tests::hash::{Test, main_test};
+use crypto_tests::hash::{Test, main_test, one_million_a};
 
 #[test]
 fn md2_main() {
     // Examples from wikipedia
     let tests = new_tests!("test1", "test2", "test3");
     main_test::<md2::Md2>(&tests);
+}
+
+#[test]
+fn md2_1million_a() {
+    let output = include_bytes!("data/one_million_a.output.bin");
+    one_million_a::<md2::Md2>(output);
 }
