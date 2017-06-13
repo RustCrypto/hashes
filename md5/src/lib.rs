b19bb8e5353ea870bbe278c5647484f35df78130
Artyom Pavlov <newpavlov@gmail.com>
2017-06-13 12:48:18 +0300

    Removed generic-array dep fro md5 and sha1

--- md5/src/lib.rs
+++ md5/src/lib.rs
@@ -3,19 +3,12 @@
 //! For full MD5 hash function with this implementation of compression function
 //! use [md-5](https://crates.io/crates/md-5) crate with
 //! the enabled "asm" feature.
-//! 
+//!
 //! Only x86 and x86-64 architectures are currently supported.
-//! 
+//!
 //! [1]: https://en.wikipedia.org/wiki/MD5
 
 #![no_std]
-extern crate generic_array;
-
-use generic_array::GenericArray;
-use generic_array::typenum::U64;
-use core::mem;
-
-type Block = GenericArray<u8, U64>;
 
 #[link(name="md5", kind="static")]
 extern "C" {
@@ -24,8 +17,8 @@ extern "C" {
 
 /// Safe wrapper around assembly implementation of MD5 compression function
 #[inline]
-pub fn compress(state: &mut [u32; 4], block: &Block) {
+pub fn compress(state: &mut [u32; 4], block: &[u8; 64]) {
     unsafe {
-        md5_compress(state, mem::transmute(block));
+        md5_compress(state, block);
     }
 }
--- sha1/src/lib.rs
+++ sha1/src/lib.rs
@@ -3,20 +3,12 @@
 //! For full SHA-1 hash function with this implementation of compression function
 //! use [sha-1](https://crates.io/crates/sha-1) crate with
 //! the enabled "asm" feature.
-//! 
+//!
 //! Only x86 and x86-64 architectures are currently supported.
-//! 
+//!
 //! [1]: https://en.wikipedia.org/wiki/SHA-1
 
 #![no_std]
-extern crate generic_array;
-
-use generic_array::GenericArray;
-use generic_array::typenum::U64;
-use core::mem;
-
-type BlockSize = U64;
-type Block = GenericArray<u8, BlockSize>;
 
 #[link(name="sha1", kind="static")]
 extern "C" {
@@ -25,8 +17,6 @@ extern "C" {
 
 /// Safe wrapper around assembly implementation of SHA-1 compression function
 #[inline]
-pub fn compress(state: &mut [u32; 5], block: &Block) {
-    unsafe {
-        sha1_compress(state, mem::transmute(block));
-    }
+pub fn compress(state: &mut [u32; 5], block: &[u8; 64]) {
+    unsafe { sha1_compress(state, block); }
 }
