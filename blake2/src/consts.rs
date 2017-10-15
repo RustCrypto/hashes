afecbe40aefa7fe5b4aa513693f1daf1c99e6a18
Kyle Galloway <kyle.galloway.1993@gmail.com>
2017-10-15 16:11:50 -0500

    Fix clippy warning in blake2 (except inline_always)

--- blake2/src/consts.rs
+++ blake2/src/consts.rs
@@ -14,10 +14,10 @@ pub static SIGMA : [[usize; 16]; 12] = [
 ];
 
 pub static BLAKE2B_IV : [u64; 8] = [
-    0x6a09e667f3bcc908, 0xbb67ae8584caa73b,
-    0x3c6ef372fe94f82b, 0xa54ff53a5f1d36f1,
-    0x510e527fade682d1, 0x9b05688c2b3e6c1f,
-    0x1f83d9abfb41bd6b, 0x5be0cd19137e2179,
+    0x6a09_e667_f3bc_c908, 0xbb67_ae85_84ca_a73b,
+    0x3c6e_f372_fe94_f82b, 0xa54f_f53a_5f1d_36f1,
+    0x510e_527f_ade6_82d1, 0x9b05_688c_2b3e_6c1f,
+    0x1f83_d9ab_fb41_bd6b, 0x5be0_cd19_137e_2179,
 ];
 
 /*
@@ -29,8 +29,8 @@ pub const BLAKE2B_PERSONALBYTES : usize = 16;
 */
 
 pub static BLAKE2S_IV : [u32; 8] = [
-    0x6A09E667, 0xBB67AE85, 0x3C6EF372, 0xA54FF53A,
-    0x510E527F, 0x9B05688C, 0x1F83D9AB, 0x5BE0CD19,
+    0x6A09_E667, 0xBB67_AE85, 0x3C6E_F372, 0xA54F_F53A,
+    0x510E_527F, 0x9B05_688C, 0x1F83_D9AB, 0x5BE0_CD19,
 ];
 
 /*
