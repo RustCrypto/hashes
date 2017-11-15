c56282a8020f091e631165d2e1fa9de590303b03
newpavlov <newpavlov@gmail.com>
2017-11-15 18:44:57 +0300

    blake2 doc updates

--- blake2/src/lib.rs
+++ blake2/src/lib.rs
@@ -1,16 +1,16 @@
 //! An implementation of the [BLAKE2][1] hash functions.
 //!
-//! Based on the [work][2] of Cesar Barros.
+//! Based on the [blake2-rfc][2] crate.
 //!
 //! # Usage
 //!
-//! An example of using `Blake2b` is:
+//! `Blake2b` can be used in the following way:
 //!
 //! ```rust
 //! use blake2::{Blake2b, Digest};
 //!
 //! // create a Blake2b object
-//! let mut hasher = Blake2b::default();
+//! let mut hasher = Blake2b::new();
 //!
 //! // write input message
 //! hasher.input(b"hello world");
@@ -20,15 +20,58 @@
 //! println!("{:x}", output);
 //! ```
 //!
-//! Same for `Blake2s`:
+//! Same example for `Blake2s`:
 //!
 //! ```rust
 //! use blake2::{Blake2s, Digest};
 //!
-//! let mut hasher = Blake2s::default();
+//! let mut hasher = Blake2s::new();
 //! hasher.input(b"hello world");
 //! let output = hasher.result();
 //! println!("{:x}", output);
+//! ```
+//!
+//! ## Variable output size
+//!
+//! Both `Blake2b` and `Blake2s` support variable output sizes through
+//! `VariableOutput` trait. `Input` trait has to be imported as well.
+//!
+//! ```rust
+//! use blake2::Blake2b;
+//! use blake2::digest::{Input, VariableOutput};
+//!
+//! let mut hasher = Blake2b::new(10).unwrap();
+//! // instead of `input` method here we should use `process`
+//! hasher.process(b"my_input");
+//! let mut buf = [0u8; 10];
+//! hasher.variable_result(&mut buf).unwrap();
+//! assert_eq!(buf, [44, 197, 92, 132, 228, 22, 146, 78, 100, 0])
+//! ```
+//!
+//! ## Message Authentication Code (MAC)
+//!
+//! BLAKE2 can be used as a MAC without any additionall constructs:
+//!
+//! ```rust
+//! use blake2::Blake2b;
+//! use blake2::crypto_mac::Mac;
+//!
+//! let mut hasher = Blake2b::new(b"my key").unwrap();
+//! hasher.input(b"hello world");
+//!
+//! // `result` has type `MacResult` which is a thin wrapper around array of
+//! // bytes for providing constant time equality check
+//! let result = hasher.result();
+//! // To get underlying array use `code` method, but be carefull, since
+//! // incorrect use of the code value may permit timing attacks which defeat
+//! // the security provided by the `MacResult`
+//! let code_bytes = result.code();
+//!
+//! // To verify the message it's recommended to use `verify` method
+//! let mut hasher = Blake2b::new(b"my key").unwrap();
+//! hasher.input(b"hello world");
+//! // `verify` return `Ok(())` if code is correct, `Err(MacError)` otherwise
+//! hasher.verify(&code_bytes).unwrap();
 //! ```
 //!
 //! [1]: https://en.wikipedia.org/wiki/BLAKE_(hash_function)#BLAKE2
@@ -42,8 +85,8 @@
 
 extern crate byte_tools;
 #[macro_use]
-extern crate digest;
-extern crate crypto_mac;
+pub extern crate digest;
+pub extern crate crypto_mac;
 
 mod consts;
 mod as_bytes;
@@ -58,6 +101,5 @@ mod blake2b;
 mod blake2s;
 
 pub use digest::Digest;
-pub use crypto_mac::Mac;
 pub use blake2b::Blake2b;
 pub use blake2s::Blake2s;
