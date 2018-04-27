3f111719b72a048a24104cdfdbf7c7d2500a4fe6
newpavlov <newpavlov@gmail.com>
2018-04-27 11:17:22 +0300

    removed generic-array dependency

--- sha2/src/lib.rs
+++ sha2/src/lib.rs
@@ -3,17 +3,12 @@
 //! For full SHA-2 hash functions with this implementation of compression
 //! functions use [sha-2](https://crates.io/crates/sha-2) crate with
 //! the enabled "asm" feature.
-//! 
+//!
 //! Only x86 and x86-64 architectures are currently supported.
-//! 
+//!
 //! [1]: https://en.wikipedia.org/wiki/SHA-2
 
 #![no_std]
-extern crate generic_array;
-
-use generic_array::GenericArray;
-use generic_array::typenum::{U64, U128};
-use core::mem;
 
 #[link(name="sha256", kind="static")]
 extern "C" {
@@ -22,10 +17,8 @@ extern "C" {
 
 /// Safe wrapper around assembly implementation of SHA256 compression function
 #[inline]
-pub fn compress256(state: &mut [u32; 8], block: &GenericArray<u8, U64>) {
-    unsafe {
-        sha256_compress(state, mem::transmute(block));
-    }
+pub fn compress256(state: &mut [u32; 8], block: &[u8; 64]) {
+    unsafe { sha256_compress(state, block) }
 }
 
 #[link(name="sha512", kind="static")]
@@ -35,8 +28,6 @@ extern "C" {
 
 /// Safe wrapper around assembly implementation of SHA512 compression function
 #[inline]
-pub fn compress512(state: &mut [u64; 8], block: &GenericArray<u8, U128>) {
-    unsafe {
-        sha512_compress(state, mem::transmute(block));
-    }
+pub fn compress512(state: &mut [u64; 8], block: &[u8; 128]) {
+    unsafe { sha512_compress(state, block) }
 }
--- whirlpool/src/lib.rs
+++ whirlpool/src/lib.rs
@@ -3,19 +3,12 @@
 //! For full Whirlpool hash function with this implementation of compression
 //! function use [whirlpool](https://crates.io/crates/whirlpool) crate with
 //! the enabled "asm" feature.
-//! 
+//!
 //! Only x86 and x86-64 architectures are currently supported.
-//! 
+//!
 //! [1]: https://en.wikipedia.org/wiki/Whirlpool_(cryptography)
 
 #![no_std]
-extern crate generic_array;
-
-use generic_array::GenericArray;
-use generic_array::typenum::U64;
-use core::mem;
-
-type Block = GenericArray<u8, U64>;
 
 #[link(name="whirlpool", kind="static")]
 extern "C" {
@@ -24,8 +17,6 @@ extern "C" {
 
 /// Safe wrapper around assembly implementation of Whirlpool compression function
 #[inline]
-pub fn compress(state: &mut Block, block: &Block) {
-    unsafe {
-        whirlpool_compress(mem::transmute(state), mem::transmute(block));
-    }
+pub fn compress(state: &mut [u8; 64], block: &[u8; 64]) {
+    unsafe { whirlpool_compress(state, block) }
 }
