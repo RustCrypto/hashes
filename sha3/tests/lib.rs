8efbd02798ef60beb1e31891a055bdaccf079fcb
newpavlov <newpavlov@gmail.com>
2018-08-31 21:10:04 +0300

    update test files

--- sha3/tests/lib.rs
+++ sha3/tests/lib.rs
@@ -15,9 +15,5 @@ new_test!(sha3_256, "sha3_256", sha3::Sha3_256, digest_test);
 new_test!(sha3_384, "sha3_384", sha3::Sha3_384, digest_test);
 new_test!(sha3_512, "sha3_512", sha3::Sha3_512, digest_test);
 
-new_test!(shake128_1, "shake128_1", sha3::Shake128, xof_test);
-new_test!(shake128_2, "shake128_2", sha3::Shake128, xof_test);
-new_test!(shake128_3, "shake128_3", sha3::Shake128, xof_test);
-new_test!(shake256_1, "shake256_1", sha3::Shake256, xof_test);
-new_test!(shake256_2, "shake256_2", sha3::Shake256, xof_test);
-new_test!(shake256_3, "shake256_3", sha3::Shake256, xof_test);
+new_test!(shake128, "shake128", sha3::Shake128, xof_test);
+new_test!(shake256, "shake256", sha3::Shake256, xof_test);
