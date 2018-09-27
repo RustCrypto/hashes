47c95ee238898092ef7b870aa6f6069a6545deac
Kaz Wesley <kaz@lambdaverse.org>
2018-09-27 14:17:22 -0700

    update block-buffer 0.3 -> 0.6

--- jh/src/lib.rs
+++ jh/src/lib.rs
@@ -9,8 +9,10 @@ pub extern crate digest;
 
 pub use digest::Digest;
 
-use block_buffer::BlockBuffer512;
-use digest::generic_array::GenericArray;
+use block_buffer::byteorder::BigEndian;
+use block_buffer::BlockBuffer;
+use block_buffer::generic_array::GenericArray as BBGenericArray;
+use digest::generic_array::GenericArray as DGenericArray;
 use digest::generic_array::typenum::{U28, U32, U48, U64, Unsigned};
 
 mod consts;
@@ -24,8 +26,8 @@ extern "C" {
 }
 
 impl State {
-    fn process_block(&mut self, block: &[u8; 64]) {
-        unsafe { f8(self, block) };
+    fn process_block(&mut self, block: &BBGenericArray<u8, U64>) {
+        unsafe { f8(self, block.as_slice().as_ptr() as *const [u8; 64]) };
     }
 }
 
@@ -37,22 +39,12 @@ impl core::fmt::Debug for State {
     }
 }
 
-struct DigestPadToEnd {}
-impl block_buffer::Padding for DigestPadToEnd {
-    fn pad(block: &mut [u8], pos: usize) {
-        block[pos] = 0x80;
-        if pos+1 < block.len() {
-            for x in &mut block[pos+1..] { *x = 0 };
-        }
-    }
-}
-
 macro_rules! define_hasher {
     ($name:ident, $init:path, $OutputBytes:ident) => {
         #[derive(Clone)]
         pub struct $name {
             state: State,
-            buffer: BlockBuffer512,
+            buffer: BlockBuffer<U64>,
             datalen: usize,
         }
 
@@ -60,7 +52,7 @@ macro_rules! define_hasher {
             fn fmt(&self, f: &mut core::fmt::Formatter) -> Result<(), core::fmt::Error> {
                 f.debug_struct("Jh")
                     .field("state", &self.state)
-                    .field("buffer", &"(BlockBuffer512)")
+                    .field("buffer", &"(BlockBuffer<U64>)")
                     .field("datalen", &self.datalen)
                     .finish()
             }
@@ -70,7 +62,7 @@ macro_rules! define_hasher {
             fn default() -> Self {
                 Self {
                     state: State(*$init),
-                    buffer: BlockBuffer512::default(),
+                    buffer: BlockBuffer::default(),
                     datalen: 0
                 }
             }
@@ -91,15 +83,16 @@ macro_rules! define_hasher {
         impl digest::FixedOutput for $name {
             type OutputSize = $OutputBytes;
 
-            fn fixed_result(mut self) -> GenericArray<u8, $OutputBytes> {
+            fn fixed_result(mut self) -> DGenericArray<u8, $OutputBytes> {
                 let state = &mut self.state;
                 let buffer = &mut self.buffer;
                 let len = self.datalen as u64 * 8;
                 if buffer.position() == 0 {
-                    buffer.len_padding(len.to_be(), |b| state.process_block(b));
+                    buffer.len64_padding::<BigEndian, _>(len, |b| state.process_block(b));
                 } else {
-                    state.process_block(buffer.pad_with::<DigestPadToEnd>());
-                    let mut last = [0u8; 64];
+                    use block_buffer::block_padding::Iso7816;
+                    state.process_block(buffer.pad_with::<Iso7816>().unwrap());
+                    let mut last = BBGenericArray::default();
                     last[56] = (len >> 56) as u8;
                     last[57] = (len >> 48) as u8;
                     last[58] = (len >> 40) as u8;
@@ -110,7 +103,7 @@ macro_rules! define_hasher {
                     last[63] = len as u8;
                     state.process_block(&last);
                 }
-                let mut out = GenericArray::default();
+                let mut out = DGenericArray::default();
                 out.copy_from_slice(&state.0[(128 - $OutputBytes::to_usize())..]);
                 out
             }
