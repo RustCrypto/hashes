84b05daa914c5248dd9b6a79b97f066e0f3f20bf
Kaz Wesley <kaz@lambdaverse.org>
2018-10-20 11:03:36 -0700

    skein: simplify tests

--- skein/tests/lib.rs
+++ skein/tests/lib.rs
@@ -1,83 +1,60 @@
-#![no_std]
-#[macro_use]
 extern crate digest;
 extern crate skein;
 
-use digest::dev::{main_test, Test};
+use digest::Digest;
 use digest::generic_array::typenum::{U32, U64};
 
-#[test]
-fn skein_256_32_0() {
-    let tests = new_tests!("skein_256/test32_0");
-    main_test::<skein::Skein256<U32>>(&tests);
+fn read_files(path: &str) -> (Vec<u8>, Vec<u8>) {
+    use std::io::Read;
+    let mut input = std::fs::File::open(format!("tests/data/{}.input.bin", path)).unwrap();
+    let mut output = std::fs::File::open(format!("tests/data/{}.output.bin", path)).unwrap();
+    let mut buf = Vec::new();
+    input.read_to_end(&mut buf).unwrap();
+    let input = buf.clone();
+    buf.clear();
+    output.read_to_end(&mut buf).unwrap();
+    let output = buf;
+    (input, output)
 }
 
 #[test]
-fn skein_256_32_17() {
-    let tests = new_tests!("skein_256/test32_17");
-    main_test::<skein::Skein256<U32>>(&tests);
+fn skein_256() {
+    for path in &[ "skein_256/test32_0", "skein_256/test32_17", "skein_256/test32_64" ] {
+        let (input, output) = read_files(path);
+        let hash = skein::Skein256::<U32>::digest(&input);
+        assert_eq!(&hash[..], &output[..])
+    }
+    for path in &[ "skein_256/test64_0", "skein_256/test64_17", "skein_256/test64_64" ] {
+        let (input, output) = read_files(path);
+        let hash = skein::Skein256::<U64>::digest(&input);
+        assert_eq!(&hash[..], &output[..])
+    }
 }
 
 #[test]
-fn skein_256_32_64() {
-    let tests = new_tests!("skein_256/test32_64");
-    main_test::<skein::Skein256<U32>>(&tests);
+fn skein_512() {
+    for path in &[ "skein_512/test32_0", "skein_512/test32_17", "skein_512/test32_64" ] {
+        let (input, output) = read_files(path);
+        let hash = skein::Skein512::<U32>::digest(&input);
+        assert_eq!(&hash[..], &output[..])
+    }
+    for path in &[ "skein_512/test64_0", "skein_512/test64_17", "skein_512/test64_64" ] {
+        let (input, output) = read_files(path);
+        let hash = skein::Skein512::<U64>::digest(&input);
+        assert_eq!(&hash[..], &output[..])
+    }
 }
 
 #[test]
-fn skein_256_64_0() {
-    let tests = new_tests!("skein_256/test64_0");
-    main_test::<skein::Skein256<U64>>(&tests);
-}
-
-#[test]
-fn skein_256_64_17() {
-    let tests = new_tests!("skein_256/test64_17");
-    main_test::<skein::Skein256<U64>>(&tests);
-}
-
-#[test]
-fn skein_256_64_64() {
-    let tests = new_tests!("skein_256/test64_64");
-    main_test::<skein::Skein256<U64>>(&tests);
-}
-
-#[test]
-fn skein_512_32() {
-    let tests = new_tests!(
-        "skein_512/test32_0",
-        "skein_512/test32_17",
-        "skein_512/test32_64",
-    );
-    main_test::<skein::Skein512<U32>>(&tests);
-}
-
-#[test]
-fn skein_512_64() {
-    let tests = new_tests!(
-        "skein_512/test64_0",
-        "skein_512/test64_17",
-        "skein_512/test64_64",
-    );
-    main_test::<skein::Skein512<U64>>(&tests);
-}
-
-#[test]
-fn skein_1024_32() {
-    let tests = new_tests!(
-        "skein_1024/test32_0",
-        "skein_1024/test32_17",
-        "skein_1024/test32_64",
-    );
-    main_test::<skein::Skein1024<U32>>(&tests);
-}
-
-#[test]
-fn skein_1024_64() {
-    let tests = new_tests!(
-        "skein_1024/test64_0",
-        "skein_1024/test64_17",
-        "skein_1024/test64_64",
-    );
-    main_test::<skein::Skein1024<U64>>(&tests);
+fn skein_1024() {
+    for path in &[ "skein_1024/test32_0", "skein_1024/test32_17", "skein_1024/test32_64" ] {
+        let (input, output) = read_files(path);
+        let hash = skein::Skein1024::<U32>::digest(&input);
+        assert_eq!(&hash[..], &output[..])
+    }
+    for path in &[ "skein_1024/test64_0", "skein_1024/test64_17", "skein_1024/test64_64" ] {
+        let (input, output) = read_files(path);
+        let hash = skein::Skein1024::<U64>::digest(&input);
+        assert_eq!(&hash[..], &output[..])
+    }
 }
