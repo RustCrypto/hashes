f20d61d74bbad682cc958cc20fd06b8cb58aafc5
Kaz Wesley <kaz@lambdaverse.org>
2018-10-20 10:32:11 -0700

    simplify tests

--- jh/tests/lib.rs
+++ jh/tests/lib.rs
@@ -1,51 +1,31 @@
-#![no_std]
-extern crate jh_x86_64;
-#[macro_use]
-extern crate digest;
+use digest::Digest;
 
-use digest::dev::{main_test, Test};
+fn read_files(path: &str) -> (Vec<u8>, Vec<u8>) {
+    let input = std::fs::read(format!("tests/data/{}.input.bin", path)).unwrap();
+    let output = std::fs::read(format!("tests/data/{}.output.bin", path)).unwrap();
+    (input, output)
+}
 
 #[test]
 fn jh_224_0() {
-    let tests = new_tests!("jh_224/test_0");
-    main_test::<jh_x86_64::Jh224>(&tests);
-}
-
-#[test]
-fn jh_224_17() {
-    let tests = new_tests!("jh_224/test_17");
-    main_test::<jh_x86_64::Jh224>(&tests);
-}
-
-#[test]
-fn jh_224_64() {
-    let tests = new_tests!("jh_224/test_64");
-    main_test::<jh_x86_64::Jh224>(&tests);
-}
-
-#[test]
-fn jh_224_123() {
-    let tests = new_tests!("jh_224/test_123");
-    main_test::<jh_x86_64::Jh224>(&tests);
-}
-
-#[test]
-fn jh_256() {
-    let tests = new_tests!("jh_256/test_0", "jh_256/test_17",
-                           "jh_256/test_64", "jh_256/test_123");
-    main_test::<jh_x86_64::Jh256>(&tests);
-}
-
-#[test]
-fn jh_384() {
-    let tests = new_tests!("jh_384/test_0", "jh_384/test_17",
-                           "jh_384/test_64", "jh_384/test_123");
-    main_test::<jh_x86_64::Jh384>(&tests);
-}
-
-#[test]
-fn jh_512() {
-    let tests = new_tests!("jh_512/test_0", "jh_512/test_17",
-                           "jh_512/test_64", "jh_512/test_123");
-    main_test::<jh_x86_64::Jh512>(&tests);
+    for path in &[ "jh_224/test_0", "jh_224/test_17", "jh_224/test_64", "jh_224/test_123" ] {
+        let (input, output) = read_files(path);
+        let hash = jh_x86_64::Jh224::digest(&input);
+        assert_eq!(&hash[..], &output[..]);
+    }
+    for path in &[ "jh_256/test_0", "jh_256/test_17", "jh_256/test_64", "jh_256/test_123" ] {
+        let (input, output) = read_files(path);
+        let hash = jh_x86_64::Jh256::digest(&input);
+        assert_eq!(&hash[..], &output[..]);
+    }
+    for path in &[ "jh_384/test_0", "jh_384/test_17", "jh_384/test_64", "jh_384/test_123" ] {
+        let (input, output) = read_files(path);
+        let hash = jh_x86_64::Jh384::digest(&input);
+        assert_eq!(&hash[..], &output[..]);
+    }
+    for path in &[ "jh_512/test_0", "jh_512/test_17", "jh_512/test_64", "jh_512/test_123" ] {
+        let (input, output) = read_files(path);
+        let hash = jh_x86_64::Jh512::digest(&input);
+        assert_eq!(&hash[..], &output[..]);
+    }
 }
