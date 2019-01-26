5372ba6eeb093c63a467b157fb82b202ead91372
Kaz Wesley <kaz@lambdaverse.org>
2019-01-26 11:39:22 -0800

    use digest-dev for tests

--- skein/tests/lib.rs
+++ skein/tests/lib.rs
@@ -1,60 +1,14 @@
-extern crate digest;
+#![no_std]
 extern crate skein;
+#[macro_use]
+extern crate digest;
 
-use digest::Digest;
+use digest::dev::digest_test;
 use digest::generic_array::typenum::{U32, U64};
 
-fn read_files(path: &str) -> (Vec<u8>, Vec<u8>) {
-    use std::io::Read;
-    let mut input = std::fs::File::open(format!("tests/data/{}.input.bin", path)).unwrap();
-    let mut output = std::fs::File::open(format!("tests/data/{}.output.bin", path)).unwrap();
-    let mut buf = Vec::new();
-    input.read_to_end(&mut buf).unwrap();
-    let input = buf.clone();
-    buf.clear();
-    output.read_to_end(&mut buf).unwrap();
-    let output = buf;
-    (input, output)
-}
-
-#[test]
-fn skein_256() {
-    for path in &[ "skein_256/test32_0", "skein_256/test32_17", "skein_256/test32_64" ] {
-        let (input, output) = read_files(path);
-        let hash = skein::Skein256::<U32>::digest(&input);
-        assert_eq!(&hash[..], &output[..])
-    }
-    for path in &[ "skein_256/test64_0", "skein_256/test64_17", "skein_256/test64_64" ] {
-        let (input, output) = read_files(path);
-        let hash = skein::Skein256::<U64>::digest(&input);
-        assert_eq!(&hash[..], &output[..])
-    }
-}
-
-#[test]
-fn skein_512() {
-    for path in &[ "skein_512/test32_0", "skein_512/test32_17", "skein_512/test32_64" ] {
-        let (input, output) = read_files(path);
-        let hash = skein::Skein512::<U32>::digest(&input);
-        assert_eq!(&hash[..], &output[..])
-    }
-    for path in &[ "skein_512/test64_0", "skein_512/test64_17", "skein_512/test64_64" ] {
-        let (input, output) = read_files(path);
-        let hash = skein::Skein512::<U64>::digest(&input);
-        assert_eq!(&hash[..], &output[..])
-    }
-}
-
-#[test]
-fn skein_1024() {
-    for path in &[ "skein_1024/test32_0", "skein_1024/test32_17", "skein_1024/test32_64" ] {
-        let (input, output) = read_files(path);
-        let hash = skein::Skein1024::<U32>::digest(&input);
-        assert_eq!(&hash[..], &output[..])
-    }
-    for path in &[ "skein_1024/test64_0", "skein_1024/test64_17", "skein_1024/test64_64" ] {
-        let (input, output) = read_files(path);
-        let hash = skein::Skein1024::<U64>::digest(&input);
-        assert_eq!(&hash[..], &output[..])
-    }
-}
+new_test!(skein256_32, "skein256_32", skein::Skein256<U32>, digest_test);
+new_test!(skein512_32, "skein512_32", skein::Skein512<U32>, digest_test);
+new_test!(skein1024_32, "skein1024_32", skein::Skein1024<U32>, digest_test);
+new_test!(skein256_64, "skein256_64", skein::Skein256<U64>, digest_test);
+new_test!(skein512_64, "skein512_64", skein::Skein512<U64>, digest_test);
+new_test!(skein1024_64, "skein1024_64", skein::Skein1024<U64>, digest_test);
