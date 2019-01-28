208ea26617c5f4b0f93ba2e1beb0fc9f2900b591
Kaz Wesley <kaz@lambdaverse.org>
2019-01-28 11:04:02 -0800

    tests from NIST submission

--- jh/tests/lib.rs
+++ jh/tests/lib.rs
@@ -1,31 +1,16 @@
-use digest::Digest;
+#![no_std]
+#[macro_use]
+extern crate digest;
+extern crate jh_x86_64;
 
-fn read_files(path: &str) -> (Vec<u8>, Vec<u8>) {
-    let input = std::fs::read(format!("tests/data/{}.input.bin", path)).unwrap();
-    let output = std::fs::read(format!("tests/data/{}.output.bin", path)).unwrap();
-    (input, output)
-}
+use digest::dev::digest_test;
 
-#[test]
-fn jh_224_0() {
-    for path in &[ "jh_224/test_0", "jh_224/test_17", "jh_224/test_64", "jh_224/test_123" ] {
-        let (input, output) = read_files(path);
-        let hash = jh_x86_64::Jh224::digest(&input);
-        assert_eq!(&hash[..], &output[..]);
-    }
-    for path in &[ "jh_256/test_0", "jh_256/test_17", "jh_256/test_64", "jh_256/test_123" ] {
-        let (input, output) = read_files(path);
-        let hash = jh_x86_64::Jh256::digest(&input);
-        assert_eq!(&hash[..], &output[..]);
-    }
-    for path in &[ "jh_384/test_0", "jh_384/test_17", "jh_384/test_64", "jh_384/test_123" ] {
-        let (input, output) = read_files(path);
-        let hash = jh_x86_64::Jh384::digest(&input);
-        assert_eq!(&hash[..], &output[..]);
-    }
-    for path in &[ "jh_512/test_0", "jh_512/test_17", "jh_512/test_64", "jh_512/test_123" ] {
-        let (input, output) = read_files(path);
-        let hash = jh_x86_64::Jh512::digest(&input);
-        assert_eq!(&hash[..], &output[..]);
-    }
-}
+new_test!(long_224, "LongMsgKAT_224", jh_x86_64::Jh224, digest_test);
+new_test!(long_256, "LongMsgKAT_256", jh_x86_64::Jh256, digest_test);
+new_test!(long_384, "LongMsgKAT_384", jh_x86_64::Jh384, digest_test);
+new_test!(long_512, "LongMsgKAT_512", jh_x86_64::Jh512, digest_test);
+
+new_test!(short_224, "ShortMsgKAT_224", jh_x86_64::Jh224, digest_test);
+new_test!(short_256, "ShortMsgKAT_256", jh_x86_64::Jh256, digest_test);
+new_test!(short_384, "ShortMsgKAT_384", jh_x86_64::Jh384, digest_test);
+new_test!(short_512, "ShortMsgKAT_512", jh_x86_64::Jh512, digest_test);
