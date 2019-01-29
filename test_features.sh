5b4ea99bb4fcaf89c1903fdb6c9115143401f39d
Kaz Wesley <kaz@lambdaverse.org>
2019-01-29 10:49:37 -0800

    test coresimd on travis
    This will test the sse2 and probably ssse3 coresimd implementations. If
    the buildbox happens to support avx2, it will also test the avx2
    implementations.

--- test_features.sh
+++ test_features.sh
@@ -3,4 +3,7 @@ cd sha1 && cargo test --features asm && cd .. &&
 cd whirlpool && cargo test --features asm && cd .. &&
 cd blake2 && cargo test --features simd &&
              cargo test --features simd_opt &&
-             cargo test --features simd_asm
+             cargo test --features simd_asm &&
+             RUSTFLAGS="-Ctarget-cpu=native" cargo test --features coresimd &&
+             RUSTFLAGS="-Ctarget-cpu=native -Ctarget-feature=-ssse3" cargo test --features coresimd &&
+             RUSTFLAGS="-Ctarget-cpu=native -Ctarget-feature=-avx2" cargo test --features coresimd
