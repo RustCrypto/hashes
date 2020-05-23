627696dc38643d185f3716a08f7ec1a90a1a30c0
Tony Arcieri <bascule@gmail.com>
2020-05-23 16:59:09 -0700

    sha2 v0.8.2

--- sha2/src/lib.rs
+++ sha2/src/lib.rs
@@ -54,6 +54,7 @@
 //!
 //! [1]: https://en.wikipedia.org/wiki/SHA-2
 //! [2]: https://github.com/RustCrypto/hashes
+
 #![no_std]
 #![doc(html_logo_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo_small.png")]
 
