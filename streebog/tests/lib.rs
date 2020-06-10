a6b4b022ecfb962d7b1cbf8cb85bf3731f76ea60
Artyom Pavlov <newpavlov@gmail.com>
2020-06-10 12:01:41 +0000

    typo fix

--- streebog/tests/lib.rs
+++ streebog/tests/lib.rs
@@ -1,7 +1,7 @@
 use digest::dev::digest_test;
 use digest::new_test;
 
-// Tests vectors from: https://github.com/gost-engine/engine
+// Test vectors from: https://github.com/gost-engine/engine
 
 new_test!(
     streebog256_main,
