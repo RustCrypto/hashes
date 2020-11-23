f08856ddebca33ec8037669be371fe7e1d9b0015
Xiang Li <lxiange@cmu.edu>
2020-11-23 12:16:17 -0800

    Fix using sha2_asm on aarch64.
    The code doesn't compile before.
    Can use `cargo test --features asm` to verify.
    Tested on AWS Graviton2 server.

--- sha2/src/sha256/aarch64.rs
+++ sha2/src/sha256/aarch64.rs
@@ -12,9 +12,9 @@ pub fn compress(state: &mut [u32; 8], blocks: &[[u8; 64]]) {
     if sha2_supported() {
         // TODO: replace after sha2-asm rework
         for block in blocks {
-            sha2_asm::compress256(&mut self.h, block);
+            sha2_asm::compress256(state, block);
         }
     } else {
-        super::soft::compress(&mut self.h, block);
+        super::soft::compress(state, blocks);
     }
 }
