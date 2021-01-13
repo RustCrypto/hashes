646ef9a1b1b8ccfa9233be648955f47e1c52da89
Артём Павлов <newpavlov@gmail.com>
2021-01-13 23:39:33 +0300

    remove the last comment

--- sha2/src/sha256/aarch64.rs
+++ sha2/src/sha256/aarch64.rs
@@ -10,7 +10,6 @@ pub fn compress(state: &mut [u32; 8], blocks: &[[u8; 64]]) {
     // TODO: Replace with https://github.com/rust-lang/rfcs/pull/2725
     // after stabilization
     if sha2_supported() {
-        // TODO: replace after sha2-asm rework
         for block in blocks {
             sha2_asm::compress256(state, block);
         }
