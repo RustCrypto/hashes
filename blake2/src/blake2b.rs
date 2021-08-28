0ce9be69db8643f27f8a66df5c44d79aec1cfcbe
Tony Arcieri <bascule@gmail.com>
2021-08-28 17:59:58 -0600

    blake2: extract `Hash` types into modules
    Clearing out the `blake2b` and `blake2s` modules in order to add the
    primary user-facing types there.

--- blake2/src/blake2b.rs
+++ blake2/src/blake2b.rs
@@ -27,11 +27,12 @@ pub(crate) mod backend;
 pub mod many;
 pub(crate) mod state;
 
+mod hash;
 mod params;
 #[cfg(test)]
 mod test;
 
-pub use self::{params::Params, state::State};
+pub use self::{hash::Hash, params::Params, state::State};
 
 use core::{fmt, mem::size_of};
 
@@ -94,76 +95,6 @@ const SIGMA: [[u8; 16]; 12] = [
 /// ```
 pub fn blake2b(input: &[u8]) -> Hash {
     Params::new().hash(input)
-}
-
-type HexString = arrayvec::ArrayString<[u8; 2 * OUTBYTES]>;
-
-/// A finalized BLAKE2 hash, with constant-time equality.
-#[derive(Clone, Copy)]
-pub struct Hash {
-    pub(crate) bytes: [u8; OUTBYTES],
-    pub(crate) len: u8,
-}
-
-impl Hash {
-    /// Convert the hash to a byte slice. Note that if you're using BLAKE2 as a MAC, you need
-    /// constant time equality, which `&[u8]` doesn't provide.
-    pub fn as_bytes(&self) -> &[u8] {
-        &self.bytes[..self.len as usize]
-    }
-
-    /// Convert the hash to a byte array. Note that if you're using BLAKE2 as a
-    /// MAC, you need constant time equality, which arrays don't provide. This
-    /// panics in debug mode if the length of the hash isn't `OUTBYTES`.
-    #[inline]
-    pub fn as_array(&self) -> &[u8; OUTBYTES] {
-        debug_assert_eq!(self.len as usize, OUTBYTES);
-        &self.bytes
-    }
-
-    /// Convert the hash to a lowercase hexadecimal
-    /// [`ArrayString`](https://docs.rs/arrayvec/0.4/arrayvec/struct.ArrayString.html).
-    pub fn to_hex(self) -> HexString {
-        bytes_to_hex(self.as_bytes())
-    }
-}
-
-fn bytes_to_hex(bytes: &[u8]) -> HexString {
-    let mut s = arrayvec::ArrayString::new();
-    let table = b"0123456789abcdef";
-    for &b in bytes {
-        s.push(table[(b >> 4) as usize] as char);
-        s.push(table[(b & 0xf) as usize] as char);
-    }
-    s
-}
-
-/// This implementation is constant time, if the two hashes are the same length.
-impl PartialEq for Hash {
-    fn eq(&self, other: &Hash) -> bool {
-        constant_time_eq::constant_time_eq(self.as_bytes(), other.as_bytes())
-    }
-}
-
-/// This implementation is constant time, if the slice is the same length as the hash.
-impl PartialEq<[u8]> for Hash {
-    fn eq(&self, other: &[u8]) -> bool {
-        constant_time_eq::constant_time_eq(self.as_bytes(), other)
-    }
-}
-
-impl Eq for Hash {}
-
-impl AsRef<[u8]> for Hash {
-    fn as_ref(&self) -> &[u8] {
-        self.as_bytes()
-    }
-}
-
-impl fmt::Debug for Hash {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "Hash(0x{})", self.to_hex())
-    }
 }
 
 // Paint a byte pattern that won't repeat, so that we don't accidentally miss
