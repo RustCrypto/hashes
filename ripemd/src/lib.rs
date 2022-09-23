4561d6fbafeb979a797db9d40d301a1f354dbe40
Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
2022-09-23 13:23:10 +0300

    ripemd: Add OID support (#415)

--- ripemd/src/lib.rs
+++ ripemd/src/lib.rs
@@ -49,6 +49,8 @@
 pub use digest::{self, Digest};
 
 use core::fmt;
+#[cfg(feature = "oid")]
+use digest::const_oid::{AssociatedOid, ObjectIdentifier};
 use digest::{
     block_buffer::Eager,
     core_api::{
@@ -158,3 +160,27 @@ impl_ripemd!(Ripemd128Core, Ripemd128, c128, "128", "RIPEMD-128", U16);
 impl_ripemd!(Ripemd160Core, Ripemd160, c160, "160", "RIPEMD-160", U20);
 impl_ripemd!(Ripemd256Core, Ripemd256, c256, "256", "RIPEMD-256", U32);
 impl_ripemd!(Ripemd320Core, Ripemd320, c320, "320", "RIPEMD-320", U40);
+
+#[cfg(feature = "oid")]
+#[cfg_attr(docsrs, doc(cfg(feature = "oid")))]
+impl AssociatedOid for Ripemd128Core {
+    /// The OID used for the RIPEMD-160. There are two OIDs defined. The Teletrust one (which is
+    /// used by almost anybody, including BouncyCastle, OpenSSL, GnuTLS, etc. and the ISO one
+    /// (1.0.10118.3.0.50), which seems to be used by nobody.
+    const OID: ObjectIdentifier = ObjectIdentifier::new_unwrap("1.3.36.3.2.2");
+}
+
+#[cfg(feature = "oid")]
+#[cfg_attr(docsrs, doc(cfg(feature = "oid")))]
+impl AssociatedOid for Ripemd160Core {
+    /// The OID used for the RIPEMD-160. There are two OIDs defined. The Teletrust one (which is
+    /// used by almost anybody, including BouncyCastle, OpenSSL, GnuTLS, etc. and the ISO one
+    /// (1.0.10118.3.0.49), which seems to be used by Go and nobody else.
+    const OID: ObjectIdentifier = ObjectIdentifier::new_unwrap("1.3.36.3.2.1");
+}
+
+#[cfg(feature = "oid")]
+#[cfg_attr(docsrs, doc(cfg(feature = "oid")))]
+impl AssociatedOid for Ripemd256Core {
+    const OID: ObjectIdentifier = ObjectIdentifier::new_unwrap("1.3.36.3.2.3");
+}
