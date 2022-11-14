b0a768eb78831f06284983181be040d622639612
Артём Павлов <newpavlov@gmail.com>
2022-11-14 13:35:51 +0300

    fix test MSRV

--- blake2/tests/mac.rs
+++ blake2/tests/mac.rs
@@ -11,14 +11,14 @@ fn blake2b_new_test() {
     use blake2::digest::{generic_array::GenericArray, KeyInit, Mac};
 
     fn run<T: Mac + KeyInit>(key: &[u8]) {
-        let data = [42; 300];
+        const DATA: &[u8] = &[42; 300];
         let res1 = <T as Mac>::new(GenericArray::from_slice(key))
-            .chain_update(&data)
+            .chain_update(DATA)
             .finalize()
             .into_bytes();
         let res2 = <T as Mac>::new_from_slice(&key)
             .unwrap()
-            .chain_update(&data)
+            .chain_update(DATA)
             .finalize()
             .into_bytes();
         assert_eq!(res1, res2);
