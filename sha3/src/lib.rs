6f426a2ed987ba5848d4c481c03f30c31fd2e279
Alexander Wagner <alexander.wagner@ulal.de>
2023-03-15 14:29:05 +0100

    sha3: Add TurboSHAKE
    This implementation is based on the specification at
     https://eprint.iacr.org/2023/342
    and the IETF draft at
     https://www.ietf.org/id/draft-irtf-cfrg-kangarootwelve-09.html

--- sha3/src/lib.rs
+++ sha3/src/lib.rs
@@ -93,6 +93,9 @@ const SHA3: u8 = 0x06;
 const SHAKE: u8 = 0x1f;
 const CSHAKE: u8 = 0x4;
 
+// Round counts
+const TURBO_SHAKE_ROUND_COUNT: usize = 12;
+
 impl_sha3!(Keccak224Core, Keccak224, U28, U144, KECCAK, "Keccak-224");
 impl_sha3!(Keccak256Core, Keccak256, U32, U136, KECCAK, "Keccak-256");
 impl_sha3!(Keccak384Core, Keccak384, U48, U104, KECCAK, "Keccak-384");
@@ -163,6 +166,25 @@ impl_shake!(
     SHAKE,
     "SHAKE256",
     "2.16.840.1.101.3.4.2.11",
+);
+
+impl_turbo_shake!(
+    TurboShake128Core,
+    TurboShake128,
+    TurboShake128ReaderCore,
+    TurboShake128Reader,
+    U168,
+    "TurboSHAKE128",
+    "",
+);
+impl_turbo_shake!(
+    TurboShake256Core,
+    TurboShake256,
+    TurboShake256ReaderCore,
+    TurboShake256Reader,
+    U136,
+    "TurboSHAKE256",
+    "",
 );
 
 impl_cshake!(
--- sha3/src/state.rs
+++ sha3/src/state.rs
@@ -1,13 +1,31 @@
 use core::convert::TryInto;
 
 const PLEN: usize = 25;
+const DEFAULT_ROUND_COUNT: usize = 24;
 
-#[derive(Clone, Default)]
+#[derive(Clone)]
 pub(crate) struct Sha3State {
     pub state: [u64; PLEN],
+    round_count: usize,
+}
+
+impl Default for Sha3State {
+    fn default() -> Self {
+        Self {
+            state: [0u64; PLEN],
+            round_count: DEFAULT_ROUND_COUNT,
+        }
+    }
 }
 
 impl Sha3State {
+    pub(crate) fn new(round_count: usize) -> Self {
+        Self {
+            state: [0u64; PLEN],
+            round_count,
+        }
+    }
+
     #[inline(always)]
     pub(crate) fn absorb_block(&mut self, block: &[u8]) {
         debug_assert_eq!(block.len() % 8, 0);
@@ -16,7 +34,7 @@ impl Sha3State {
             *s ^= u64::from_le_bytes(b.try_into().unwrap());
         }
 
-        keccak::f1600(&mut self.state);
+        keccak::keccak_p(&mut self.state, self.round_count);
     }
 
     #[inline(always)]
@@ -27,7 +45,7 @@ impl Sha3State {
     }
 
     #[inline(always)]
-    pub(crate) fn apply_f(&mut self) {
-        keccak::f1600(&mut self.state);
+    pub(crate) fn permute(&mut self) {
+        keccak::keccak_p(&mut self.state, self.round_count);
     }
 }
