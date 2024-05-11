ca7590b9bdc6e095290b2de7a5f8d6892c0d9d00
Arthur Gautier <arthur.gautier@arista.com>
2024-05-10 23:36:14 -0700

    blake2: fixup rustc 1.78 warnings
    Starting 1.78, rust will report the `as_mut_bytes` method is dead code.
    
    I believe rustc is correct.

--- blake2/src/as_bytes.rs
+++ blake2/src/as_bytes.rs
@@ -13,18 +13,12 @@ pub unsafe trait Safe {}
 
 pub trait AsBytes {
     fn as_bytes(&self) -> &[u8];
-    fn as_mut_bytes(&mut self) -> &mut [u8];
 }
 
 impl<T: Safe> AsBytes for [T] {
     #[inline]
     fn as_bytes(&self) -> &[u8] {
         unsafe { slice::from_raw_parts(self.as_ptr() as *const u8, mem::size_of_val(self)) }
-    }
-
-    #[inline]
-    fn as_mut_bytes(&mut self) -> &mut [u8] {
-        unsafe { slice::from_raw_parts_mut(self.as_mut_ptr() as *mut u8, mem::size_of_val(self)) }
     }
 }
 
