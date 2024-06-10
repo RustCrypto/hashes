afcad8ae4b789c0a07d41c2f60504f3eeaec4450
Anders Steen <anders.steen@knowit.no>
2024-06-10 21:52:22 +0200

    Fixing bug

--- multimixer-128/src/lib.rs
+++ multimixer-128/src/lib.rs
@@ -1,33 +1,23 @@
 use core::fmt;
-use std::io::Read;
 
 use digest::crypto_common::{InvalidLength, Key, KeyInit, KeySizeUser};
 pub use digest::{self, Digest};
 
-use digest::typenum::{array, Unsigned};
+use digest::typenum::Unsigned;
 use digest::{
-    array::Array,
-    consts::{U256, U32, U64, U8},
+    consts::{U32, U64},
     core_api::{
         AlgorithmName, Block, BlockSizeUser, BufferKindUser, CoreWrapper, FixedOutputCore,
         UpdateCore,
     },
-    HashMarker, OutputSizeUser, Reset,
+    HashMarker, OutputSizeUser,
 };
 
 const BLOCKSIZE: usize = 32;
 
 pub struct MultimixerCore {
-    x: [u32; 4usize],
-    y: [u32; 4usize],
-    h: [u32; 4usize],
-    k: [u32; 4usize],
-    a: [u32; 4usize],
-    b: [u32; 4usize],
-    p: [u32; 4usize],
-    q: [u32; 4usize],
     key_blocks: Vec<Block<Self>>,
-    block_sum: [u64; 8usize],
+    block_sums: [u64; 8usize],
     block_index: usize,
 }
 
@@ -36,37 +26,84 @@ pub type Multimixer = CoreWrapper<MultimixerCore>;
 impl MultimixerCore {
     fn compress(&mut self, message_block: &Block<Self>) {
         //self.x[0] = message_block & 0xffffff_000000_000000_000000_000000_000000_000000_000000;
+
+        let mut x: [u32; 4usize] = [0u32; 4];
+        let mut h = [0u32; 4];
+        let mut y = [0u32; 4];
+        let mut k = [0u32; 4];
+        let mut a = [0u32; 4];
+        let mut b = [0u32; 4];
+        let mut p = [0u32; 4];
+        let mut q = [0u32; 4];
+
         for i in 0..4 {
-            self.x[i] = u32::from_be_bytes([
+            x[i] = u32::from_ne_bytes([
                 message_block[0 + i * 4],
                 message_block[1 + i * 4],
                 message_block[2 + i * 4],
                 message_block[3 + i * 4],
             ]);
-            self.y[i] = u32::from_be_bytes([
+            y[i] = u32::from_ne_bytes([
                 message_block[16 + i * 4],
                 message_block[17 + i * 4],
                 message_block[18 + i * 4],
                 message_block[19 + i * 4],
             ]);
-            self.h[i] = u32::from_be_bytes([
+            h[i] = u32::from_ne_bytes([
                 self.key_blocks[self.block_index][i * 4],
                 self.key_blocks[self.block_index][i * 4 + 1],
                 self.key_blocks[self.block_index][i * 4 + 2],
                 self.key_blocks[self.block_index][i * 4 + 3],
             ]);
-            self.k[i] = u32::from_be_bytes([
+            k[i] = u32::from_ne_bytes([
                 self.key_blocks[self.block_index][i * 4 + 16],
                 self.key_blocks[self.block_index][i * 4 + 17],
                 self.key_blocks[self.block_index][i * 4 + 18],
                 self.key_blocks[self.block_index][i * 4 + 19],
             ]);
 
-            self.a[i] = (self.x[i] + self.h[i]) % (2u32.pow(BLOCKSIZE as u32));
-            self.b[i] = (self.y[i] + self.k[i]) % (2u32.pow(BLOCKSIZE as u32));
+            a[i] = x[i].wrapping_add(h[i]);
+            b[i] = y[i].wrapping_add(k[i]);
         }
-        // TODO: impl p, q, block_sum
+
+        for i in 0..4 {
+            p[i] = a[i]
+                .wrapping_add(a[(i + 1) % 4])
+                .wrapping_add(a[(i + 2) % 4]);
+            q[i] = b[(i + 1) % 4]
+                .wrapping_add(b[(i + 2) % 4])
+                .wrapping_add(b[(i + 3) % 4]);
+        }
+
+        let block_res = [
+            a[0] as u64 * b[0] as u64,
+            a[1] as u64 * b[1] as u64,
+            a[2] as u64 * b[2] as u64,
+            a[3] as u64 * b[3] as u64,
+            p[0] as u64 * q[0] as u64,
+            p[1] as u64 * q[1] as u64,
+            p[2] as u64 * q[2] as u64,
+            p[3] as u64 * q[3] as u64,
+        ];
+
+        // let mut block_temp = [0u64; 8usize];
+        // Update blk_temp with the results from Blk_res
+        for i in 0..self.block_sums.len() {
+            self.block_sums[i] = self.block_sums[i].wrapping_add(block_res[i]);
+        }
+        println!("block_sums: {:02x?}", self.block_sums);
+        println!("x: {:x?}", x);
+        println!("y: {:x?}", y);
         self.block_index += 1;
+    }
+
+    fn finalize(&self, out: &mut digest::Output<Self>) {
+        for (i, block) in self.block_sums.iter().enumerate() {
+            let bytes = block.to_le_bytes(); // Convert u64 to little-endian byte array
+            for (j, &byte) in bytes.iter().enumerate() {
+                out[i * 8 + j] = byte;
+            }
+        }
     }
 }
 
@@ -85,15 +122,7 @@ impl KeyInit for MultimixerCore {
             return Err(InvalidLength);
         }
         let mut s = Self {
-            x: [0; 4],
-            y: [0; 4],
-            h: [0; 4],
-            k: [0; 4],
-            a: [0; 4],
-            b: [0; 4],
-            p: [0; 4],
-            q: [0; 4],
-            block_sum: [0; 8],
+            block_sums: [0; 8],
             key_blocks: Vec::new(),
             block_index: 0,
         };
@@ -123,10 +152,10 @@ impl BufferKindUser for MultimixerCore {
 }
 
 impl OutputSizeUser for MultimixerCore {
-    type OutputSize = U8;
+    type OutputSize = U64;
 
     fn output_size() -> usize {
-        8usize
+        64usize
     }
 }
 
@@ -144,34 +173,15 @@ impl FixedOutputCore for MultimixerCore {
         buffer: &mut digest::core_api::Buffer<Self>,
         out: &mut digest::Output<Self>,
     ) {
-        let pos = buffer.get_pos();
-        let rem = buffer.remaining() as u8;
-        let mut block = buffer.pad_with_zeros();
-        block[pos..].iter_mut().for_each(|b| *b = rem);
+        //let pos = buffer.get_pos();
+        //let rem = buffer.remaining() as u8;
+        //let mut block = buffer.pad_with_zeros();
+        //block[pos..].iter_mut().for_each(|b| *b = rem);
 
-        self.compress(&block);
+        //self.compress(&block);
+        self.finalize(out);
     }
 }
-
-/*
-impl Default for MultimixerCore {
-    fn default() -> Self {
-        Self {
-            x: [0; 4usize],
-            y: [0; 4usize],
-            h: [0; 4usize],
-            a: [0; 4usize],
-            b: [0; 4usize],
-            p: [0; 4usize],
-            q: [0; 4usize],
-            block_sum: [0; 8usize],
-        }
-    }
-}
-impl Reset for MultimixerCore {
-    fn reset(&mut self) {}
-}
-*/
 
 impl AlgorithmName for MultimixerCore {
     fn write_alg_name(f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
@@ -184,20 +194,3 @@ impl fmt::Debug for MultimixerCore {
         f.write_str("MultimixerCore { ... }")
     }
 }
-/*
-impl Clone for MultimixerCore {
-    fn clone(&self) -> Self {
-        Self {
-            x: self.x.clone(),
-            y: self.y.clone(),
-            h: self.h.clone(),
-            a: self.a.clone(),
-            b: self.b.clone(),
-            p: self.p.clone(),
-            q: self.q.clone(),
-            key: self.key.clone(),
-            block_sum: self.block_sum.clone(),
-        }
-    }
-}
-*/
--- multimixer-128/tests/mod.rs
+++ multimixer-128/tests/mod.rs
@@ -1,5 +1,5 @@
+use digest::crypto_common::KeyInit;
 use digest::FixedOutput;
-use digest::KeyInit;
 use digest::Update;
 use hex_literal::hex;
 
@@ -8,13 +8,25 @@ use multimixer_128::Multimixer;
 // digest::new_test!(toy_main, "toy", Toy, fixed_reset_test); Need to find out how to make .blb testdata.
 
 #[test]
-fn multimixer_test() {
-    let key = [0x42u8; 32];
-    let data = [0x69u8; 32];
-    let mut h = Multimixer::new(&key.into());
-    h.update(&data);
+fn multimixer_simple_test() {
+    let key = &hex!("4420823cfde6f1c26b30f90ec7dd01e4887534a20f0b0d04c36ed80e71e0fd77");
+    let message = &hex!("b07670eb940bd5335f973daad8619b91ffc911f57cced458bbbf2ce03753c9bd");
+    let mut h = Multimixer::new(key.into());
+    h.update(message);
     assert_eq!(
         h.finalize_fixed().as_slice(),
-        &hex!("c42ec87c23422526bf90f16eb222ee6e")[..]
+        &hex!("aca6cbf6480d9b17bb9d13efbb3589596ca1ce7d3ae4edac586d77e22313b5189f6c97c2a910636df227850c398ca01b92ab25c1ccec360e4020eec91331a383")[..]
+    );
+}
+
+#[test]
+fn multimixer_simple_160_test() {
+    let key = &hex!("0702f5a3c49364cc514d0f07c64a1dc2824228ec9b07121f42158c3cdd2e610eff428e62e5c7a889857c7d1e59b3db1fb4d366d9238825805a314d1e68db161b2ef0bd32a0144010e241cae40c8a2e80a62b9a11c41d85a04285c23b9b30d97d69a9adc8f63542e50f955066bdc7a631d1b040211699a0d598a3b48ba6043e4ca2a6a723e78ff5e8bac2281c4418fb807dadb9bdce9dedae550e4b807144395e");
+    let message = &hex!("d21932883668852228256f58dd0bbcf9917066fc78d9e7bb60f62583d06704c2f927ced914b4ea036199023d9aa190d2d19de79a43e347538104d912bcd7cd90092e2e02c489ed8bbef6acc6e93bf7b54ad44b095885bc4193d38493d78cddabf86efbcdd92e2042694c750d34814ff532cc5f012dda1a6fd8b11834d63c878e5bf5186d2cc73fe596fec93bf5364cc5675583d593fc6dacf83404b1881ce199");
+    let mut h = Multimixer::new_from_slice(key).unwrap();
+    h.update(message);
+    assert_eq!(
+        h.finalize_fixed().as_slice(),
+        &hex!("fa80bf97fff5b9b014b0691c27907b1f04ac2debd24f964b9ae546d269d6eca934762c68a377114213591c04a762bb331eafe51633c06ee7304fc8dca2c88604")[..]
     );
 }
