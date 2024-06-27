fa5c07723cfac54420aed2f1ad7deb7c2a2ab7d3
AnarchistHoneybun <thatonebipanda@gmail.com>
2024-06-27 22:47:53 +0530

    modified to address clippy warnings

--- kupyna512/src/sub_units/mod.rs
+++ kupyna512/src/sub_units/mod.rs
@@ -13,11 +13,9 @@ fn silo(message_block: &[u8], prev_vector: &[u8]) -> Vec<u8> {
 
     let t_xor_mp = t_xor_l(&m_xor_p, ROUNDS);
 
-    let t_plus_m = t_plus_l(&message_block, ROUNDS);
+    let t_plus_m = t_plus_l(message_block, ROUNDS);
 
-    let return_vector = xor_bytes(&(xor_bytes(&t_xor_mp, &t_plus_m)), prev_vector);
-
-    return_vector
+    xor_bytes(&(xor_bytes(&t_xor_mp, &t_plus_m)), prev_vector)
 }
 
 pub(crate) fn plant(message_blocks: Vec<&[u8]>, init_vector: &[u8]) -> Vec<u8> {
@@ -27,13 +25,10 @@ pub(crate) fn plant(message_blocks: Vec<&[u8]>, init_vector: &[u8]) -> Vec<u8> {
         last_vector = silo(block, &last_vector);
     }
 
-    let last_vector = finalize(&last_vector);
-
-    last_vector
+    finalize(&last_vector)
 }
 
 fn finalize(ult_processed_block: &[u8]) -> Vec<u8> {
     let t_xor_ult_processed_block = t_xor_l(ult_processed_block, ROUNDS);
-    let final_state = xor_bytes(ult_processed_block, &t_xor_ult_processed_block);
-    final_state
+    xor_bytes(ult_processed_block, &t_xor_ult_processed_block)
 }
