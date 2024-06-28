bf41d89cec755991645542ef07660e3d3bd85bbe
Joshua Koudys <jkoudys@gmail.com>
2024-06-27 22:29:10 -0400

    fix: update correct crate name

--- kupyna/src/main.rs
+++ kupyna/src/main.rs
@@ -1,10 +1,10 @@
-use kupyna_512;
+use kupyna;
 
 fn main() {
     let message = b"Hello, World!".to_vec();
     let _message_length = 0;
 
-    let hash_code = kupyna_512::hash(message, None).unwrap();
+    let hash_code = kupyna::hash(message, None).unwrap();
 
     println!("{:02X?}", hash_code);
 }
