96b4322c1636d8b22775f82c8b89c6979d3e8e07
AnarchistHoneybun <thatonebipanda@gmail.com>
2024-10-11 20:20:22 +0530

    passing borrow of struct further downstream to eliminate all constant dependencies

--- kupyna/src/sub_units/mod.rs
+++ kupyna/src/sub_units/mod.rs
@@ -12,9 +12,9 @@ fn xor_bytes(a: &[u8], b: &[u8]) -> Vec<u8> {
 fn silo(message_block: &[u8], prev_vector: &[u8], hash_params: &KupynaH) -> Vec<u8> {
     let m_xor_p = xor_bytes(message_block, prev_vector);
 
-    let t_xor_mp = t_xor_l(&m_xor_p, ROUNDS);
+    let t_xor_mp = t_xor_l(&m_xor_p, hash_params);
 
-    let t_plus_m = t_plus_l(message_block, ROUNDS);
+    let t_plus_m = t_plus_l(message_block, hash_params);
 
     xor_bytes(&(xor_bytes(&t_xor_mp, &t_plus_m)), prev_vector)
 }
@@ -30,6 +30,6 @@ pub(crate) fn plant(message_blocks: Vec<&[u8]>, init_vector: &[u8], hash_params:
 }
 
 fn finalize(ult_processed_block: &[u8], hash_params: &KupynaH) -> Vec<u8> {
-    let t_xor_ult_processed_block = t_xor_l(ult_processed_block, ROUNDS);
+    let t_xor_ult_processed_block = t_xor_l(ult_processed_block, hash_params);
     xor_bytes(ult_processed_block, &t_xor_ult_processed_block)
 }
