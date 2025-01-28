46384d66a6cbe721bde71ebf8777baa480970bc8
Артём Павлов [Artyom Pavlov] <newpavlov@gmail.com>
2025-01-28 22:38:59 +0300

    Fix blake2 typo

--- blake2/tests/mod.rs
+++ blake2/tests/mod.rs
@@ -1,19 +1,24 @@
 #[cfg(feature = "reset")]
-use digest::dev::{fixed_reset_test as fixed_fn, variable_reset_test as varaible_fn};
+use digest::dev::{fixed_reset_test as fixed_test, variable_reset_test as variable_fn};
 #[cfg(not(feature = "reset"))]
-use digest::dev::{fixed_test as fixed_fn, variable_test as varaible_fn};
+use digest::dev::{fixed_test, variable_test};
 use digest::new_test;
 
-new_test!(blake2b_fixed, "blake2b/fixed", blake2::Blake2b512, fixed_fn,);
+new_test!(
+    blake2b_fixed,
+    "blake2b/fixed",
+    blake2::Blake2b512,
+    fixed_test,
+);
 new_test!(
     blake2b_variable,
     "blake2b/variable",
     blake2::Blake2bVar,
-    varaible_fn,
+    variable_test,
 );
 new_test!(
     blake2s_variable,
     "blake2s/variable",
     blake2::Blake2sVar,
-    varaible_fn,
+    variable_test,
 );
