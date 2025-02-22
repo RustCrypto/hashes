f75eb2980dd60eac2b88fb375d3ded1a45498d98
Артём Павлов [Artyom Pavlov] <newpavlov@gmail.com>
2025-02-22 22:47:21 +0300

    tweak sha1

--- sha1/src/compress/x86.rs
+++ sha1/src/compress/x86.rs
@@ -31,64 +31,63 @@ macro_rules! schedule_rounds4 {
 }
 
 #[target_feature(enable = "sha,sse2,ssse3,sse4.1")]
+#[allow(unsafe_op_in_unsafe_fn)]
 unsafe fn digest_blocks(state: &mut [u32; 5], blocks: &[[u8; 64]]) {
-    unsafe {
-        #[allow(non_snake_case)]
-        let MASK: __m128i = _mm_set_epi64x(0x0001_0203_0405_0607, 0x0809_0A0B_0C0D_0E0F);
+    #[allow(non_snake_case)]
+    let MASK: __m128i = _mm_set_epi64x(0x0001_0203_0405_0607, 0x0809_0A0B_0C0D_0E0F);
 
-        let mut state_abcd = _mm_loadu_si128(state.as_ptr().cast());
-        state_abcd = _mm_shuffle_epi32(state_abcd, 0b00011011);
-        let mut state_e = _mm_set_epi32(state[4] as i32, 0, 0, 0);
+    let mut state_abcd = _mm_loadu_si128(state.as_ptr().cast());
+    state_abcd = _mm_shuffle_epi32(state_abcd, 0b00011011);
+    let mut state_e = _mm_set_epi32(state[4] as i32, 0, 0, 0);
 
-        for block in blocks {
-            let block_ptr: *const __m128i = block.as_ptr().cast();
+    for block in blocks {
+        let block_ptr: *const __m128i = block.as_ptr().cast();
 
-            let mut w0 = _mm_shuffle_epi8(_mm_loadu_si128(block_ptr.offset(0)), MASK);
-            let mut w1 = _mm_shuffle_epi8(_mm_loadu_si128(block_ptr.offset(1)), MASK);
-            let mut w2 = _mm_shuffle_epi8(_mm_loadu_si128(block_ptr.offset(2)), MASK);
-            let mut w3 = _mm_shuffle_epi8(_mm_loadu_si128(block_ptr.offset(3)), MASK);
-            #[allow(clippy::needless_late_init)]
-            let mut w4;
+        let mut w0 = _mm_shuffle_epi8(_mm_loadu_si128(block_ptr.offset(0)), MASK);
+        let mut w1 = _mm_shuffle_epi8(_mm_loadu_si128(block_ptr.offset(1)), MASK);
+        let mut w2 = _mm_shuffle_epi8(_mm_loadu_si128(block_ptr.offset(2)), MASK);
+        let mut w3 = _mm_shuffle_epi8(_mm_loadu_si128(block_ptr.offset(3)), MASK);
+        #[allow(clippy::needless_late_init)]
+        let mut w4;
 
-            let mut h0 = state_abcd;
-            let mut h1 = _mm_add_epi32(state_e, w0);
+        let mut h0 = state_abcd;
+        let mut h1 = _mm_add_epi32(state_e, w0);
 
-            // Rounds 0..20
-            h1 = _mm_sha1rnds4_epu32(h0, h1, 0);
-            h0 = rounds4!(h1, h0, w1, 0);
-            h1 = rounds4!(h0, h1, w2, 0);
-            h0 = rounds4!(h1, h0, w3, 0);
-            schedule_rounds4!(h0, h1, w0, w1, w2, w3, w4, 0);
+        // Rounds 0..20
+        h1 = _mm_sha1rnds4_epu32(h0, h1, 0);
+        h0 = rounds4!(h1, h0, w1, 0);
+        h1 = rounds4!(h0, h1, w2, 0);
+        h0 = rounds4!(h1, h0, w3, 0);
+        schedule_rounds4!(h0, h1, w0, w1, w2, w3, w4, 0);
 
-            // Rounds 20..40
-            schedule_rounds4!(h1, h0, w1, w2, w3, w4, w0, 1);
-            schedule_rounds4!(h0, h1, w2, w3, w4, w0, w1, 1);
-            schedule_rounds4!(h1, h0, w3, w4, w0, w1, w2, 1);
-            schedule_rounds4!(h0, h1, w4, w0, w1, w2, w3, 1);
-            schedule_rounds4!(h1, h0, w0, w1, w2, w3, w4, 1);
+        // Rounds 20..40
+        schedule_rounds4!(h1, h0, w1, w2, w3, w4, w0, 1);
+        schedule_rounds4!(h0, h1, w2, w3, w4, w0, w1, 1);
+        schedule_rounds4!(h1, h0, w3, w4, w0, w1, w2, 1);
+        schedule_rounds4!(h0, h1, w4, w0, w1, w2, w3, 1);
+        schedule_rounds4!(h1, h0, w0, w1, w2, w3, w4, 1);
 
-            // Rounds 40..60
-            schedule_rounds4!(h0, h1, w1, w2, w3, w4, w0, 2);
-            schedule_rounds4!(h1, h0, w2, w3, w4, w0, w1, 2);
-            schedule_rounds4!(h0, h1, w3, w4, w0, w1, w2, 2);
-            schedule_rounds4!(h1, h0, w4, w0, w1, w2, w3, 2);
-            schedule_rounds4!(h0, h1, w0, w1, w2, w3, w4, 2);
+        // Rounds 40..60
+        schedule_rounds4!(h0, h1, w1, w2, w3, w4, w0, 2);
+        schedule_rounds4!(h1, h0, w2, w3, w4, w0, w1, 2);
+        schedule_rounds4!(h0, h1, w3, w4, w0, w1, w2, 2);
+        schedule_rounds4!(h1, h0, w4, w0, w1, w2, w3, 2);
+        schedule_rounds4!(h0, h1, w0, w1, w2, w3, w4, 2);
 
-            // Rounds 60..80
-            schedule_rounds4!(h1, h0, w1, w2, w3, w4, w0, 3);
-            schedule_rounds4!(h0, h1, w2, w3, w4, w0, w1, 3);
-            schedule_rounds4!(h1, h0, w3, w4, w0, w1, w2, 3);
-            schedule_rounds4!(h0, h1, w4, w0, w1, w2, w3, 3);
-            schedule_rounds4!(h1, h0, w0, w1, w2, w3, w4, 3);
+        // Rounds 60..80
+        schedule_rounds4!(h1, h0, w1, w2, w3, w4, w0, 3);
+        schedule_rounds4!(h0, h1, w2, w3, w4, w0, w1, 3);
+        schedule_rounds4!(h1, h0, w3, w4, w0, w1, w2, 3);
+        schedule_rounds4!(h0, h1, w4, w0, w1, w2, w3, 3);
+        schedule_rounds4!(h1, h0, w0, w1, w2, w3, w4, 3);
 
-            state_abcd = _mm_add_epi32(state_abcd, h0);
-            state_e = _mm_sha1nexte_epu32(h1, state_e);
-        }
-
-        state_abcd = _mm_shuffle_epi32(state_abcd, 0b00011011);
-        _mm_storeu_si128(state.as_mut_ptr().cast(), state_abcd);
-        state[4] = _mm_extract_epi32(state_e, 3) as u32;
+        state_abcd = _mm_add_epi32(state_abcd, h0);
+        state_e = _mm_sha1nexte_epu32(h1, state_e);
     }
+
+    state_abcd = _mm_shuffle_epi32(state_abcd, 0b00011011);
+    _mm_storeu_si128(state.as_mut_ptr().cast(), state_abcd);
+    state[4] = _mm_extract_epi32(state_e, 3) as u32;
 }
 
 cpufeatures::new!(shani_cpuid, "sha", "sse2", "ssse3", "sse4.1");
