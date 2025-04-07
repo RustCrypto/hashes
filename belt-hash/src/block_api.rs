ba0901a90a09320f1daf274fdc655f3277d0175e
Артём Павлов [Artyom Pavlov] <newpavlov@gmail.com>
2025-04-08 02:52:41 +0300

    tweak belt-hash, add serialization test

--- belt-hash/src/block_api.rs
+++ belt-hash/src/block_api.rs
@@ -1,4 +1,3 @@
-
 use belt_block::belt_block_raw;
 use core::fmt;
 use digest::{
@@ -8,8 +7,8 @@ use digest::{
         AlgorithmName, Block, BlockSizeUser, Buffer, BufferKindUser, FixedOutputCore,
         OutputSizeUser, Reset, UpdateCore,
     },
-    typenum::{U32, U56, Unsigned},
-    crypto_common::hazmat::{SerializedState, SerializableState, DeserializeStateError},
+    crypto_common::hazmat::{DeserializeStateError, SerializableState, SerializedState},
+    typenum::{U32, U64, Unsigned},
 };
 
 #[cfg(feature = "zeroize")]
@@ -30,18 +29,9 @@ pub struct BeltHashCore {
 
 impl BeltHashCore {
     fn compress_block(&mut self, block: &Block<Self>) {
-        let x1 = [
-            get_u32(block, 0),
-            get_u32(block, 1),
-            get_u32(block, 2),
-            get_u32(block, 3),
-        ];
-        let x2 = [
-            get_u32(block, 4),
-            get_u32(block, 5),
-            get_u32(block, 6),
-            get_u32(block, 7),
-        ];
+        let (x1, x2) = block.split_at(16);
+        let x1 = read_u32s(x1);
+        let x2 = read_u32s(x2);
         let (t, h) = belt_compress(x1, x2, self.h);
         self.h = h;
         self.s.iter_mut().zip(t).for_each(|(s, t)| *s ^= t);
@@ -134,53 +124,34 @@ impl Drop for BeltHashCore {
 impl ZeroizeOnDrop for BeltHashCore {}
 
 impl SerializableState for BeltHashCore {
-    type SerializedStateSize = U56;
-
-    // r: u128, 8 
-    // s: [u32; 4], 16 
-    // h: [u32; 8], 32
+    type SerializedStateSize = U64;
 
     fn serialize(&self) -> SerializedState<Self> {
         let mut dst = SerializedState::<Self>::default();
 
-        let (r_dst, tail) = dst.split_at_mut(8);
+        let (r_dst, tail) = dst.split_at_mut(16);
         let (s_dst, h_dst) = tail.split_at_mut(16);
 
-        r_dst[20..].copy_from_slice(&self.r.to_le_bytes());
+        r_dst.copy_from_slice(&self.r.to_le_bytes());
+        write_u32s(&self.s, s_dst);
+        write_u32s(&self.h, h_dst);
 
-        for (val, chunk) in self.s.iter().zip(s_dst.chunks_exact_mut(4)) {
-            chunk.copy_from_slice(&val.to_le_bytes());
-        }
-
-        for (val, chunk) in self.h.iter().zip(h_dst.chunks_exact_mut(4)) {
-            chunk.copy_from_slice(&val.to_le_bytes());
-        }
-        
         dst
     }
 
     fn deserialize(
         serialized_state: &SerializedState<Self>,
     ) -> Result<Self, DeserializeStateError> {
-        let (r_src, tail) = serialized_state.split_at(8);
+        let (r_src, tail) = serialized_state.split_at(16);
         let (s_src, h_src) = tail.split_at(16);
 
-        let r = u128::from_le_bytes(r_src.try_into().unwrap());
-
-        let mut s = [0u32; 4];
-        for (val, chunk) in s.iter_mut().zip(s_src.chunks_exact(4)) {
-            *val = u32::from_le_bytes(chunk.try_into().unwrap());
-        }
-
-        let mut h = [0u32; 8];
-        for (val, chunk) in h.iter_mut().zip(h_src.chunks_exact(4)) {
-            *val = u32::from_le_bytes(chunk.try_into().unwrap());
-        }
-
-        Ok(Self { r, s, h })
+        Ok(Self {
+            r: u128::from_le_bytes(r_src.try_into().unwrap()),
+            s: read_u32s(s_src),
+            h: read_u32s(h_src),
+        })
     }
 }
-
 
 /// Compression function described in the section 6.3.2
 #[inline(always)]
@@ -212,8 +183,21 @@ fn concat(a: [u32; 4], b: [u32; 4]) -> [u32; 8] {
 }
 
 #[inline(always)]
-fn get_u32(block: &[u8], i: usize) -> u32 {
-    u32::from_le_bytes(block[4 * i..][..4].try_into().unwrap())
+fn read_u32s<const N: usize>(src: &[u8]) -> [u32; N] {
+    assert_eq!(src.len(), 4 * N);
+
+    let mut dst = [0u32; N];
+    for (dst, src) in dst.iter_mut().zip(src.chunks_exact(4)) {
+        *dst = u32::from_le_bytes(src.try_into().unwrap());
+    }
+    dst
+}
+
+#[inline(always)]
+fn write_u32s(src: &[u32], dst: &mut [u8]) {
+    for (src, dst) in src.iter().zip(dst.chunks_exact_mut(4)) {
+        dst.copy_from_slice(&src.to_le_bytes());
+    }
 }
 
 #[inline(always)]
@@ -228,11 +212,8 @@ fn encode_r(r: u128) -> [u32; 4] {
 
 #[cfg(test)]
 mod tests {
-    use super::{belt_compress, get_u32};
+    use super::{belt_compress, read_u32s};
     use hex_literal::hex;
-
-    const ENUM4: [usize; 4] = [0, 1, 2, 3];
-    const ENUM8: [usize; 8] = [0, 1, 2, 3, 4, 5, 6, 7];
 
     /// Test vectors for the `belt-compress` functions from the
     /// specification (Table A.8).
@@ -249,13 +230,13 @@ mod tests {
             "ED2F5481 D593F40D 87FCE37D 6BC1A2E1"
             "B7D1A2CC 975C82D3 C0497488 C90D99D8"
         );
-        let x1 = ENUM4.map(|i| get_u32(x, i));
-        let x2 = ENUM4.map(|i| get_u32(x, 4 + i));
-        let x34 = ENUM8.map(|i| get_u32(x, 8 + i));
+        let x1 = read_u32s(&x[..16]);
+        let x2 = read_u32s(&x[16..32]);
+        let x34 = read_u32s(&x[32..]);
 
         let (s, y) = belt_compress(x1, x2, x34);
 
-        assert_eq!(s, ENUM4.map(|i| get_u32(expected_s, i)));
-        assert_eq!(y, ENUM8.map(|i| get_u32(expected_y, i)));
+        assert_eq!(s, read_u32s(expected_s));
+        assert_eq!(y, read_u32s(expected_y));
     }
 }
