a667dd94f997271c3fa89ffbcc841e0c0f985654
Artyom Pavlov <newpavlov@gmail.com>
2025-04-23 10:00:52 +0400

    skein: fix implementation for output sizes not multiple of 8 (#682)

--- skein/CHANGELOG.md
+++ skein/CHANGELOG.md
@@ -5,7 +5,13 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## 0.1.0 (2023-06-09)
+## 0.1.1 (2025-04-23)
+### Fixed
+- Implementation for output sizes not multiple of 8 ([#682])
+
+[#682]: https://github.com/RustCrypto/hashes/pull/682
+
+## 0.1.0 (2023-06-09) [YANKED]
 - Initial release ([#483])
 
 [#483]: https://github.com/RustCrypto/hashes/pull/483
--- skein/tests/lib.rs
+++ skein/tests/lib.rs
@@ -1,7 +1,8 @@
+use hex_literal::hex;
 use skein::{
-    consts::{U128, U32, U64},
+    consts::{U128, U32, U64, U7},
     digest::{dev::fixed_test, new_test},
-    Skein1024, Skein256, Skein512,
+    Digest, Skein1024, Skein256, Skein512,
 };
 
 new_test!(skein256_32, "skein256_32", Skein256<U32>, fixed_test);
@@ -11,3 +12,15 @@ new_test!(skein512_64, "skein512_64", Skein512<U64>, fixed_test);
 new_test!(skein1024_32, "skein1024_32", Skein1024<U32>, fixed_test);
 new_test!(skein1024_64, "skein1024_64", Skein1024<U64>, fixed_test);
 new_test!(skein1024_128, "skein1024_128", Skein1024<U128>, fixed_test);
+
+/// Regression tests for https://github.com/RustCrypto/hashes/issues/681
+#[test]
+fn skein_uncommon_sizes() {
+    let s = "hello world";
+    let h = Skein256::<U7>::digest(s);
+    assert_eq!(h[..], hex!("31bffb70f5dafe")[..]);
+    let h = Skein512::<U7>::digest(s);
+    assert_eq!(h[..], hex!("ee6004efedd69c")[..]);
+    let h = Skein1024::<U7>::digest(s);
+    assert_eq!(h[..], hex!("a2808b638681c6")[..]);
+}
