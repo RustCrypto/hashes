3fb4aa5c7ae77acfdc7cafaa38d88840d41d1f12
Shun Sakai <sorairolake@protonmail.ch>
2026-01-13 00:17:43 +0900

    k12: add `Kt256`; rename `KangarooTwelve` to `Kt128` (#756)
    This implement KT256 as specified in RFC 9861:
    
    https://www.rfc-editor.org/rfc/rfc9861.html
    
    Also, rename existing types `KangarooTwelve*` to `Kt128*` (e.g.
    `KangarooTwelveReader` to `Kt128Reader`). This is because the
    KangarooTwelve instance is specified as the KT128 instance in RFC
    9861.[1]
    
    NOTE:  For compatibility reasons, the existing types are defined as
    deprecated aliases for `Kt128*`.
    
    [1]: https://www.rfc-editor.org/rfc/rfc9861.html#section-1-3

--- k12/benches/mod.rs
+++ k12/benches/mod.rs
@@ -5,10 +5,19 @@ use digest::bench_update;
 use test::Bencher;
 
 bench_update!(
-    k12::KangarooTwelve::default();
-    k12_10 10;
-    k12_100 100;
+    k12::Kt128::default();
+    kt128_10 10;
+    kt128_100 100;
     // the bigger sizes result in OOM
-    // k12_1000 1000;
-    // k12_10000 10000;
+    // kt128_1000 1000;
+    // kt128_10000 10000;
+);
+
+bench_update!(
+    k12::Kt256::default();
+    kt256_10 10;
+    kt256_100 100;
+    // the bigger sizes result in OOM
+    // kt256_1000 1000;
+    // kt256_10000 10000;
 );
--- k12/src/lib.rs
+++ k12/src/lib.rs
@@ -18,97 +18,138 @@ use digest::{
     CollisionResistance, ExtendableOutput, HashMarker, Reset, Update, XofReader,
     block_api::{AlgorithmName, BlockSizeUser, ExtendableOutputCore, UpdateCore, XofReaderCore},
     block_buffer::{BlockBuffer, Eager, ReadBuffer},
-    consts::{U16, U128, U168},
+    consts::{U16, U32, U128, U136, U168},
 };
 
-/// `KangarooTwelve` hasher.
-#[derive(Default, Clone)]
-pub struct KangarooTwelve<'cs> {
-    core: block_api::KangarooTwelveCore<'cs>,
-    buffer: BlockBuffer<U128, Eager>,
-}
-
-impl<'cs> KangarooTwelve<'cs> {
-    /// Creates a new KangarooTwelve instance with the given customization.
-    pub fn new(customization: &'cs [u8]) -> Self {
-        Self {
-            core: block_api::KangarooTwelveCore::new(customization),
-            buffer: Default::default(),
+macro_rules! impl_k12 {
+    (
+        $name:ident, $reader_name:ident, $core_name:ident, $reader_core_name:ident, $rate:ty,
+        $alg_name:literal,
+    ) => {
+        #[doc = $alg_name]
+        #[doc = "hasher."]
+        #[derive(Default, Clone)]
+        pub struct $name<'cs> {
+            core: block_api::$core_name<'cs>,
+            buffer: BlockBuffer<U128, Eager>,
         }
-    }
-}
 
-impl fmt::Debug for KangarooTwelve<'_> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
-        f.write_str("KangarooTwelve { .. }")
-    }
-}
-
-impl AlgorithmName for KangarooTwelve<'_> {
-    fn write_alg_name(f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.write_str("KangarooTwelve")
-    }
-}
-
-impl HashMarker for KangarooTwelve<'_> {}
-
-impl BlockSizeUser for KangarooTwelve<'_> {
-    type BlockSize = U128;
-}
-
-impl Update for KangarooTwelve<'_> {
-    fn update(&mut self, data: &[u8]) {
-        let Self { core, buffer } = self;
-        buffer.digest_blocks(data, |blocks| core.update_blocks(blocks));
-    }
-}
-
-impl Reset for KangarooTwelve<'_> {
-    fn reset(&mut self) {
-        self.core.reset();
-        self.buffer.reset();
-    }
-}
-
-impl ExtendableOutput for KangarooTwelve<'_> {
-    type Reader = KangarooTwelveReader;
-
-    #[inline]
-    fn finalize_xof(mut self) -> Self::Reader {
-        Self::Reader {
-            core: self.core.finalize_xof_core(&mut self.buffer),
-            buffer: Default::default(),
+        impl<'cs> $name<'cs> {
+            #[doc = "Creates a new"]
+            #[doc = $alg_name]
+            #[doc = "instance with the given customization."]
+            pub fn new(customization: &'cs [u8]) -> Self {
+                Self {
+                    core: block_api::$core_name::new(customization),
+                    buffer: Default::default(),
+                }
+            }
         }
-    }
+
+        impl fmt::Debug for $name<'_> {
+            fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
+                f.write_str(concat!(stringify!($name), " { .. }"))
+            }
+        }
+
+        impl AlgorithmName for $name<'_> {
+            fn write_alg_name(f: &mut fmt::Formatter<'_>) -> fmt::Result {
+                f.write_str($alg_name)
+            }
+        }
+
+        impl HashMarker for $name<'_> {}
+
+        impl BlockSizeUser for $name<'_> {
+            type BlockSize = U128;
+        }
+
+        impl Update for $name<'_> {
+            fn update(&mut self, data: &[u8]) {
+                let Self { core, buffer } = self;
+                buffer.digest_blocks(data, |blocks| core.update_blocks(blocks));
+            }
+        }
+
+        impl Reset for $name<'_> {
+            fn reset(&mut self) {
+                self.core.reset();
+                self.buffer.reset();
+            }
+        }
+
+        impl ExtendableOutput for $name<'_> {
+            type Reader = $reader_name;
+
+            #[inline]
+            fn finalize_xof(mut self) -> Self::Reader {
+                Self::Reader {
+                    core: self.core.finalize_xof_core(&mut self.buffer),
+                    buffer: Default::default(),
+                }
+            }
+        }
+
+        #[cfg(feature = "zeroize")]
+        impl digest::zeroize::ZeroizeOnDrop for $name<'_> {}
+
+        #[doc = $alg_name]
+        #[doc = "XOF reader."]
+        pub struct $reader_name {
+            core: block_api::$reader_core_name,
+            buffer: ReadBuffer<$rate>,
+        }
+
+        impl fmt::Debug for $reader_name {
+            fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
+                f.write_str(concat!(stringify!($reader_name), " { .. }"))
+            }
+        }
+
+        impl XofReader for $reader_name {
+            #[inline]
+            fn read(&mut self, buffer: &mut [u8]) {
+                let Self { core, buffer: buf } = self;
+                buf.read(buffer, |block| *block = core.read_block());
+            }
+        }
+
+        #[cfg(feature = "zeroize")]
+        impl digest::zeroize::ZeroizeOnDrop for $reader_name {}
+    };
 }
 
-impl CollisionResistance for KangarooTwelve<'_> {
-    // https://www.ietf.org/archive/id/draft-irtf-cfrg-kangarootwelve-17.html#section-7-7
+impl_k12!(
+    Kt128,
+    Kt128Reader,
+    Kt128Core,
+    Kt128ReaderCore,
+    U168,
+    "KT128",
+);
+impl_k12!(
+    Kt256,
+    Kt256Reader,
+    Kt256Core,
+    Kt256ReaderCore,
+    U136,
+    "KT256",
+);
+
+impl CollisionResistance for Kt128<'_> {
+    // https://www.rfc-editor.org/rfc/rfc9861.html#section-7-7
     type CollisionResistance = U16;
 }
 
-#[cfg(feature = "zeroize")]
-impl digest::zeroize::ZeroizeOnDrop for KangarooTwelve<'_> {}
-
-/// `KangarooTwelve` XOF reader.
-pub struct KangarooTwelveReader {
-    core: block_api::KangarooTwelveReaderCore,
-    buffer: ReadBuffer<U168>,
+impl CollisionResistance for Kt256<'_> {
+    // https://www.rfc-editor.org/rfc/rfc9861.html#section-7-8
+    type CollisionResistance = U32;
 }
 
-impl fmt::Debug for KangarooTwelveReader {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
-        f.write_str("KangarooTwelveReader { .. }")
-    }
-}
+/// KT128 hasher.
+#[deprecated(since = "0.4.0-pre", note = "use `Kt128` instead")]
+pub type KangarooTwelve<'cs> = Kt128<'cs>;
 
-impl XofReader for KangarooTwelveReader {
-    #[inline]
-    fn read(&mut self, buffer: &mut [u8]) {
-        let Self { core, buffer: buf } = self;
-        buf.read(buffer, |block| *block = core.read_block());
-    }
-}
-
-#[cfg(feature = "zeroize")]
-impl digest::zeroize::ZeroizeOnDrop for KangarooTwelveReader {}
+/// KT128 XOF reader.
+#[deprecated(since = "0.4.0-pre", note = "use `Kt128Reader` instead")]
+pub type KangarooTwelveReader = Kt128Reader;
