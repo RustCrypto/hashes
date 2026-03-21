1fc74b93c31aa1967c14ca0dc03e7794b46f90dc
Mike Mulchrone <mikemulchrone987@gmail.com>
2026-03-21 14:53:02 -0400

    fixing typo

--- sha1/tests/mod.rs
+++ sha1/tests/mod.rs
@@ -34,7 +34,7 @@ fn sha1_rand() {
 ///
 
 /// A file containing the ASCII string "abc" results in a 160 bit message digest of
-/// a9993e36 4706816a ba3e2571 7850c26c 9cd0d89d.
+/// a9993e364706816aba3e25717850c26c9cd0d89d
 #[test]
 fn abc_file_has_expected_sha1_digest() {
     let unique = SystemTime::now()
@@ -52,7 +52,7 @@ fn abc_file_has_expected_sha1_digest() {
 }
 
 /// A file containing the ASCII string "abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq"
-/// results in a 160-bit message digest of 84983e44 1c3bd26e baae4aa1 f95129e5 e54670f1.
+/// results in a 160-bit message digest of 84983e441c3bd26ebaae4aa1f95129e5e54670f1.
 #[test]
 fn long_test_vector_file_has_expected_sha1_digest() {
     let unique = SystemTime::now()
@@ -71,7 +71,7 @@ fn long_test_vector_file_has_expected_sha1_digest() {
 }
 
 /// A file containing the binary-coded form of the ASCII string which consists of 1,000,000 repetitions of the character "a" results
-/// in a SHA-1 message digest of 34aa973c d4c4daa4 f61eeb2b dbad2731 6534016f.
+/// in a SHA-1 message digest of 34aa973cd4c4daa4f61eeb2bdbad27316534016f.
 #[test]
 fn one_million_a_file_has_expected_sha1_digest() {
     let unique = SystemTime::now()
