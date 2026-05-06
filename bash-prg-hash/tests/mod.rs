785f60b3d444c57783e11fb7b8cddf5999a84ea6
Артём Павлов [Artyom Pavlov] <newpavlov@gmail.com>
2026-05-06 16:23:24 +0300

    tweak `xof_test`

--- bash-prg-hash/tests/mod.rs
+++ bash-prg-hash/tests/mod.rs
@@ -1,48 +1,32 @@
 use bash_prg_hash::{BashPrgHash1282, BashPrgHash1921, BashPrgHash2562};
+use digest::ExtendableOutput;
 use digest::dev::TestVector;
-use digest::{ExtendableOutput, XofReader};
 use hex_literal::hex;
-use std::fmt::Debug;
 
-pub fn xof_test<D: ExtendableOutput + Default + Debug + Clone>(
-    &TestVector { input, output }: &TestVector,
-) -> Result<(), &'static str> {
+fn xof_test<D>(&TestVector { input, output }: &TestVector) -> Result<(), &'static str>
+where
+    D: Default + ExtendableOutput + Clone,
+{
     let mut hasher = D::default();
     let mut buf = [0u8; 1024];
     let buf = &mut buf[..output.len()];
-
     // Test that it works when accepting the message all at once
     hasher.update(input);
-    hasher.finalize_xof().read(buf);
+    let mut hasher2 = hasher.clone();
+    hasher.finalize_xof_into(buf);
     if buf != output {
         return Err("whole message");
-    }
-    buf.iter_mut().for_each(|b| *b = 0);
-
-    // Test with fresh hasher
-    let mut hasher = D::default();
-    hasher.update(input);
-    hasher.finalize_xof().read(buf);
-    if buf != output {
-        return Err("whole message after reset");
     }
     buf.iter_mut().for_each(|b| *b = 0);
 
     // Test that it works when accepting the message in chunks
     for n in 1..core::cmp::min(17, input.len()) {
         let mut hasher = D::default();
-        let mut hasher2 = D::default();
         for chunk in input.chunks(n) {
             hasher.update(chunk);
             hasher2.update(chunk);
         }
-        hasher.finalize_xof().read(buf);
-        if buf != output {
-            return Err("message in chunks");
-        }
-        buf.iter_mut().for_each(|b| *b = 0);
-
-        hasher2.finalize_xof().read(buf);
+        hasher.finalize_xof_into(buf);
         if buf != output {
             return Err("message in chunks");
         }
