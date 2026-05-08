a8180ec7a98baa6ca49df7bd6c877c80f39500ae
Артём Павлов [Artyom Pavlov] <newpavlov@gmail.com>
2026-05-08 18:22:42 +0300

    Migrate sha3

--- sha3/src/lib.rs
+++ sha3/src/lib.rs
@@ -9,94 +9,158 @@
 #![warn(missing_docs, missing_debug_implementations)]
 #![warn(unreachable_pub)]
 
-pub use digest::{self, CollisionResistance, CustomizedInit, Digest};
+pub use digest::{self, Digest};
 
-/// Block-level types
-pub mod block_api;
+use core::fmt;
+use digest::{
+    FixedOutput, FixedOutputReset, HashMarker, Output, OutputSizeUser, Reset, Update,
+    common::{
+        AlgorithmName, BlockSizeUser,
+        hazmat::{DeserializeStateError, SerializableState, SerializedState},
+    },
+    consts::{U28, U32, U48, U64, U72, U104, U136, U144, U200, U201},
+};
+#[cfg(feature = "oid")]
+mod oids;
+mod utils;
 
-use block_api::{Sha3HasherCore, Sha3ReaderCore};
-use digest::consts::{U0, U16, U28, U32, U48, U64, U72, U104, U136, U144, U168, U200};
+/// Implementation of the SHAKE function.
+pub mod shake;
 
-// Paddings
+macro_rules! impl_fixed {
+    ($(
+        $(#[$attr:meta])*
+        $name:ident($rate:literal, $rate_ty:ty, $out_len:ty, $pad:expr);
+    )*) => {$(
+        $(#[$attr])*
+        #[derive(Clone, Default)]
+        pub struct $name {
+            state: keccak::State1600,
+            cursor: sponge_cursor::SpongeCursor<$rate>,
+            keccak: keccak::Keccak,
+        }
+
+        impl Reset for $name {
+            #[inline]
+            fn reset(&mut self) {
+                self.state = Default::default();
+                self.cursor = Default::default();
+            }
+        }
+
+        impl HashMarker for $name {}
+
+        impl Update for $name {
+            #[inline]
+            fn update(&mut self, data: &[u8]) {
+                self.keccak.with_f1600(|f1600| {
+                    self.cursor.absorb_u64_le(&mut self.state, f1600, data);
+                });
+            }
+        }
+
+        impl BlockSizeUser for $name {
+            type BlockSize = $rate_ty;
+        }
+
+        impl OutputSizeUser for $name {
+            type OutputSize = $out_len;
+        }
+
+        impl FixedOutput for $name {
+            fn finalize_into(mut self, dst: &mut Output<Self>) {
+                utils::pad::<$pad, $rate>(&mut self.state, &self.cursor);
+                self.keccak.with_f1600(|f1600| {
+                    f1600(&mut self.state);
+                    utils::read_state(&mut self.state, dst);
+                });
+            }
+        }
+
+        impl FixedOutputReset for $name {
+            fn finalize_into_reset(&mut self, dst: &mut Output<Self>) {
+                utils::pad::<$pad, $rate>(&mut self.state, &self.cursor);
+                self.keccak.with_f1600(|f1600| {
+                    f1600(&mut self.state);
+                    utils::read_state(&mut self.state, dst);
+                });
+                Reset::reset(self);
+            }
+        }
+
+        impl AlgorithmName for $name {
+            #[inline]
+            fn write_alg_name(f: &mut fmt::Formatter<'_>) -> fmt::Result {
+                f.write_str(stringify!($name))
+            }
+        }
+
+        impl SerializableState for $name {
+            type SerializedStateSize = U201;
+
+            fn serialize(&self) -> SerializedState<Self> {
+                utils::serialize(&self.state, &self.cursor).into()
+            }
+
+            fn deserialize(
+                serialized_state: &SerializedState<Self>,
+            ) -> Result<Self, DeserializeStateError> {
+                utils::deserialize(serialized_state.into())
+                    .ok_or(DeserializeStateError)
+                    .map(|(state, cursor)| Self {
+                        state,
+                        cursor,
+                        keccak: Default::default(),
+                    })
+            }
+        }
+
+        impl fmt::Debug for $name {
+            #[inline]
+            fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+                f.write_str(concat!(stringify!($name), " { ... }"))
+            }
+        }
+
+        impl Drop for $name {
+            #[inline]
+            fn drop(&mut self) {
+                #[cfg(feature = "zeroize")]
+                {
+                    use digest::zeroize::Zeroize;
+                    self.state.zeroize();
+                    self.cursor.zeroize();
+                }
+            }
+        }
+
+        #[cfg(feature = "zeroize")]
+        impl digest::zeroize::ZeroizeOnDrop for $name {}
+    )*};
+}
+
 const KECCAK_PAD: u8 = 0x01;
 const SHA3_PAD: u8 = 0x06;
-const SHAKE_PAD: u8 = 0x1f;
 
-digest::buffer_fixed!(
+impl_fixed!(
     /// SHA-3-224 hasher.
-    pub struct Sha3_224(Sha3HasherCore<U144, U28, SHA3_PAD>);
-    oid: "2.16.840.1.101.3.4.2.7";
-    impl: FixedHashTraits;
-);
-digest::buffer_fixed!(
+    Sha3_224(144, U144, U28, SHA3_PAD);
     /// SHA-3-256 hasher.
-    pub struct Sha3_256(Sha3HasherCore<U136, U32, SHA3_PAD>);
-    oid: "2.16.840.1.101.3.4.2.8";
-    impl: FixedHashTraits;
-);
-digest::buffer_fixed!(
+    Sha3_256(136, U136, U32, SHA3_PAD);
     /// SHA-3-384 hasher.
-    pub struct Sha3_384(Sha3HasherCore<U104, U48, SHA3_PAD>);
-    oid: "2.16.840.1.101.3.4.2.9";
-    impl: FixedHashTraits;
-);
-digest::buffer_fixed!(
-    /// SHA-3-512 hasher.
-    pub struct Sha3_512(Sha3HasherCore<U72, U64, SHA3_PAD>);
-    oid: "2.16.840.1.101.3.4.2.10";
-    impl: FixedHashTraits;
-);
-digest::buffer_xof!(
-    /// SHAKE128 hasher.
-    pub struct Shake128(Sha3HasherCore<U168, U0, SHAKE_PAD>);
-    oid: "2.16.840.1.101.3.4.2.11";
-    impl: XofHasherTraits;
-    /// SHAKE128 XOF reader.
-    pub struct Shake128Reader(Sha3ReaderCore<U168>);
-    impl: XofReaderTraits;
-);
-digest::buffer_xof!(
-    /// SHAKE256 hasher.
-    pub struct Shake256(Sha3HasherCore<U136, U0, SHAKE_PAD>);
-    oid: "2.16.840.1.101.3.4.2.12";
-    impl: XofHasherTraits;
-    /// SHAKE256 XOF reader.
-    pub struct Shake256Reader(Sha3ReaderCore<U136>);
-    impl: XofReaderTraits;
-);
+    Sha3_384(104, U104, U48, SHA3_PAD);
+    /// SHA-3-256 hasher.
+    Sha3_512(72, U72, U64, SHA3_PAD);
 
-digest::buffer_fixed!(
-    /// SHA-3 CryptoNight variant.
-    pub struct Keccak256Full(Sha3HasherCore<U136, U200, KECCAK_PAD>);
-    impl: FixedHashTraits;
-);
-digest::buffer_fixed!(
     /// Keccak-224 hasher.
-    pub struct Keccak224(Sha3HasherCore<U144, U28, KECCAK_PAD>);
-    impl: FixedHashTraits;
-);
-digest::buffer_fixed!(
+    Keccak224(144, U144, U28, KECCAK_PAD);
     /// Keccak-256 hasher.
-    pub struct Keccak256(Sha3HasherCore<U136, U32, KECCAK_PAD>);
-    impl: FixedHashTraits;
-);
-digest::buffer_fixed!(
+    Keccak256(136, U136, U32, KECCAK_PAD);
     /// Keccak-384 hasher.
-    pub struct Keccak384(Sha3HasherCore<U104, U48, KECCAK_PAD>);
-    impl: FixedHashTraits;
-);
-digest::buffer_fixed!(
+    Keccak384(104, U104, U48, KECCAK_PAD);
     /// Keccak-512 hasher.
-    pub struct Keccak512(Sha3HasherCore<U72, U64, KECCAK_PAD>);
-    impl: FixedHashTraits;
+    Keccak512(72, U72, U64, KECCAK_PAD);
+
+    /// CryptoNight variant of SHA-3.
+    Keccak256Full(136, U136, U200, KECCAK_PAD);
 );
-
-impl CollisionResistance for Shake128 {
-    // https://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.202.pdf#page=31
-    type CollisionResistance = U16;
-}
-
-impl CollisionResistance for Shake256 {
-    // https://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.202.pdf#page=31
-    type CollisionResistance = U32;
-}
---
+++ sha3/src/shake.rs
@@ -0,0 +1,235 @@
+use core::fmt;
+use digest::{
+    CollisionResistance, ExtendableOutput, ExtendableOutputReset, HashMarker, Reset, Update,
+    XofReader,
+    common::{
+        AlgorithmName, BlockSizeUser,
+        hazmat::{DeserializeStateError, SerializableState, SerializedState},
+    },
+    consts::{U16, U32, U136, U168, U201},
+};
+use keccak::{Keccak, State1600};
+use sponge_cursor::SpongeCursor;
+
+use crate::utils;
+
+const SHAKE_PAD: u8 = 0x1F;
+
+/// SHAKE128 hasher.
+pub type Shake128 = Shake<168>;
+/// SHAKE256 hasher.
+pub type Shake256 = Shake<136>;
+
+/// SHAKE128 XOF reader.
+pub type Shake128Reader = ShakeReader<168>;
+/// SHAKE256 XOF reader.
+pub type Shake256Reader = ShakeReader<136>;
+
+/// SHAKE hasher generic over rate.
+///
+/// Rate MUST be either 168 or 136 for SHAKE128 and SHAKE256 respectively.
+#[derive(Clone)]
+pub struct Shake<const RATE: usize> {
+    state: State1600,
+    cursor: SpongeCursor<RATE>,
+    keccak: Keccak,
+}
+
+impl<const RATE: usize> Default for Shake<RATE> {
+    #[inline]
+    fn default() -> Self {
+        const { assert!(matches!(RATE, 136 | 168)) }
+
+        Self {
+            state: Default::default(),
+            cursor: Default::default(),
+            keccak: Keccak::new(),
+        }
+    }
+}
+
+impl<const RATE: usize> HashMarker for Shake<RATE> {}
+
+impl<const RATE: usize> Update for Shake<RATE> {
+    #[inline]
+    fn update(&mut self, data: &[u8]) {
+        self.keccak.with_f1600(|f1600| {
+            self.cursor.absorb_u64_le(&mut self.state, f1600, data);
+        });
+    }
+}
+
+impl<const RATE: usize> Reset for Shake<RATE> {
+    #[inline]
+    fn reset(&mut self) {
+        self.state = Default::default();
+        self.cursor = Default::default();
+    }
+}
+
+impl<const RATE: usize> ExtendableOutput for Shake<RATE> {
+    type Reader = ShakeReader<RATE>;
+
+    #[inline]
+    fn finalize_xof(mut self) -> Self::Reader {
+        utils::pad::<SHAKE_PAD, RATE>(&mut self.state, &self.cursor);
+        // Note that `ShakeReader` applies the permutation to the state before reading from it
+        Self::Reader {
+            state: self.state,
+            cursor: Default::default(),
+            keccak: self.keccak,
+        }
+    }
+}
+
+impl<const RATE: usize> ExtendableOutputReset for Shake<RATE> {
+    #[inline]
+    fn finalize_xof_reset(&mut self) -> Self::Reader {
+        utils::pad::<SHAKE_PAD, RATE>(&mut self.state, &self.cursor);
+        let state = self.state;
+        self.reset();
+        // Note that `ShakeReader` applies the permutation to the state before reading from it
+        Self::Reader {
+            state,
+            cursor: Default::default(),
+            keccak: self.keccak,
+        }
+    }
+}
+
+impl<const RATE: usize> AlgorithmName for Shake<RATE> {
+    #[inline]
+    fn write_alg_name(f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let alg_name = match RATE {
+            168 => "SHAKE128",
+            136 => "SHAKE256",
+            _ => unreachable!(),
+        };
+        f.write_str(alg_name)
+    }
+}
+
+impl<const RATE: usize> fmt::Debug for Shake<RATE> {
+    #[inline]
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let debug_str = match RATE {
+            168 => "Shake128 { ... }",
+            136 => "Shake256 { ... }",
+            _ => unreachable!(),
+        };
+        f.write_str(debug_str)
+    }
+}
+
+impl<const RATE: usize> SerializableState for Shake<RATE> {
+    type SerializedStateSize = U201;
+
+    fn serialize(&self) -> SerializedState<Self> {
+        utils::serialize(&self.state, &self.cursor).into()
+    }
+
+    fn deserialize(
+        serialized_state: &SerializedState<Self>,
+    ) -> Result<Self, DeserializeStateError> {
+        utils::deserialize(serialized_state.into())
+            .ok_or(DeserializeStateError)
+            .map(|(state, cursor)| Self {
+                state,
+                cursor,
+                keccak: Default::default(),
+            })
+    }
+}
+
+impl<const RATE: usize> Drop for Shake<RATE> {
+    #[inline]
+    fn drop(&mut self) {
+        #[cfg(feature = "zeroize")]
+        {
+            use digest::zeroize::Zeroize;
+            self.state.zeroize();
+            self.cursor.zeroize();
+        }
+    }
+}
+
+#[cfg(feature = "zeroize")]
+impl<const RATE: usize> digest::zeroize::ZeroizeOnDrop for Shake<RATE> {}
+
+/// SHAKE XOF reader generic over rate.
+#[derive(Clone)]
+pub struct ShakeReader<const RATE: usize> {
+    state: State1600,
+    cursor: SpongeCursor<RATE>,
+    keccak: Keccak,
+}
+
+impl<const RATE: usize> XofReader for ShakeReader<RATE> {
+    #[inline]
+    fn read(&mut self, buf: &mut [u8]) {
+        self.keccak.with_f1600(|f1600| {
+            self.cursor.squeeze_read_u64_le(&mut self.state, f1600, buf);
+        });
+    }
+}
+
+impl<const RATE: usize> SerializableState for ShakeReader<RATE> {
+    type SerializedStateSize = U201;
+
+    fn serialize(&self) -> SerializedState<Self> {
+        utils::serialize(&self.state, &self.cursor).into()
+    }
+
+    fn deserialize(
+        serialized_state: &SerializedState<Self>,
+    ) -> Result<Self, DeserializeStateError> {
+        utils::deserialize(serialized_state.into())
+            .ok_or(DeserializeStateError)
+            .map(|(state, cursor)| Self {
+                state,
+                cursor,
+                keccak: Default::default(),
+            })
+    }
+}
+
+impl<const RATE: usize> fmt::Debug for ShakeReader<RATE> {
+    #[inline]
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let debug_str = match RATE {
+            168 => "ShakeReader128 { ... }",
+            136 => "ShakeReader256 { ... }",
+            _ => unreachable!(),
+        };
+        f.write_str(debug_str)
+    }
+}
+
+impl<const RATE: usize> Drop for ShakeReader<RATE> {
+    #[inline]
+    fn drop(&mut self) {
+        #[cfg(feature = "zeroize")]
+        {
+            use digest::zeroize::Zeroize;
+            self.state.zeroize();
+            self.cursor.zeroize();
+        }
+    }
+}
+
+// See Section 8.3 of NIST SP 800-185:
+// https://nvlpubs.nist.gov/nistpubs/SpecialPublications/NIST.SP.800-185.pdf
+impl CollisionResistance for Shake128 {
+    type CollisionResistance = U16;
+}
+impl CollisionResistance for Shake256 {
+    type CollisionResistance = U32;
+}
+
+impl BlockSizeUser for Shake128 {
+    type BlockSize = U168;
+}
+
+impl BlockSizeUser for Shake256 {
+    type BlockSize = U136;
+}
