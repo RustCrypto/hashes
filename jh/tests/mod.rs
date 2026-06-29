e499ef4be18015a1b5e1255ef8c5aef05822e885
Samy <ap4sh@esna.bzh>
2026-06-29 15:04:31 +0200

    jh: implement SerializableState (#889)

--- jh/tests/mod.rs
+++ jh/tests/mod.rs
@@ -1,4 +1,4 @@
-use digest::{dev::fixed_test, new_test};
+use digest::{dev::fixed_test, hash_serialization_test, new_test};
 
 new_test!(jh224_long_kat, jh::Jh224, fixed_test);
 new_test!(jh256_long_kat, jh::Jh256, fixed_test);
@@ -9,3 +9,8 @@ new_test!(jh224_short_kat, jh::Jh224, fixed_test);
 new_test!(jh256_short_kat, jh::Jh256, fixed_test);
 new_test!(jh384_short_kat, jh::Jh384, fixed_test);
 new_test!(jh512_short_kat, jh::Jh512, fixed_test);
+
+hash_serialization_test!(jh224_serialization, jh::Jh224);
+hash_serialization_test!(jh256_serialization, jh::Jh256);
+hash_serialization_test!(jh384_serialization, jh::Jh384);
+hash_serialization_test!(jh512_serialization, jh::Jh512);
